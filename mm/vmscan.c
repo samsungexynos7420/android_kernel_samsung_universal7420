f6bcc4273826a1112a432bffeeec4d381d5d1baa
Mel Gorman <mgorman@suse.de>
2013-07-03 15:01:53 -0700

    mm: vmscan: check if kswapd should writepage once per pgdat scan
    Currently kswapd checks if it should start writepage as it shrinks each
    zone without taking into consideration if the zone is balanced or not.
    This is not wrong as such but it does not make much sense either.  This
    patch checks once per pgdat scan if kswapd should be writing pages.
    
    Signed-off-by: Mel Gorman <mgorman@suse.de>
    Reviewed-by: Michal Hocko <mhocko@suse.cz>
    Acked-by: Rik van Riel <riel@redhat.com>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Cc: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Cc: Jiri Slaby <jslaby@suse.cz>
    Cc: Valdis Kletnieks <Valdis.Kletnieks@vt.edu>
    Tested-by: Zlatko Calusic <zcalusic@bitsync.net>
    Cc: dormando <dormando@rydia.net>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -2998,6 +2998,13 @@ static unsigned long balance_pgdat(pg_data_t *pgdat, int order,
 		}
 
 		/*
+		 * If we're getting trouble reclaiming, start doing writepage
+		 * even in laptop mode.
+		 */
+		if (sc.priority < DEF_PRIORITY - 2)
+			sc.may_writepage = 1;
+
+		/*
 		 * Now scan the zone in the dma->highmem direction, stopping
 		 * at the last zone which needs scanning.
 		 *
@@ -3067,13 +3074,6 @@ static unsigned long balance_pgdat(pg_data_t *pgdat, int order,
 						       &nr_attempted))
 					raise_priority = false;
 			}
-
-			/*
-			 * If we're getting trouble reclaiming, start doing
-			 * writepage even in laptop mode.
-			 */
-			if (sc.priority < DEF_PRIORITY - 2)
-				sc.may_writepage = 1;
 
 			if (zone->all_unreclaimable) {
 				if (end_zone && end_zone == i)
