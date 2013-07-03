b1567d9f40c7252929a926b0954f04db81f847a6
Arun Bharadwaj <abharadw@codeaurora.org>
2013-07-03 10:35:02 -0700

    tracing/sched: Add trace events to track cpu hotplug.
    Add ftrace event trace_sched_cpu_hotplug to track cpu
    hot-add and hot-remove events.
    
    This is useful in a variety of power, performance and
    debug analysis scenarios.
    
    Change-Id: I5d202c7a229ffacc3aafb7cf9afee0b0ee7b0931
    Signed-off-by: Arun Bharadwaj <abharadw@codeaurora.org>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -23,6 +23,7 @@
 #ifdef CONFIG_ARM_EXYNOS_MP_CPUFREQ
 #include <mach/cpufreq.h>
 #endif
+#include <trace/events/sched.h>
 
 #include "smpboot.h"
 
@@ -360,6 +361,7 @@ static int __ref _cpu_down(unsigned int cpu, int tasks_frozen)
 
 out_release:
 	cpu_hotplug_done();
+	trace_sched_cpu_hotplug(cpu, err, 0);
 	if (!err)
 		cpu_notify_nofail(CPU_POST_DEAD | mod, hcpu);
 	return err;
@@ -435,6 +437,7 @@ out_notify:
 		__cpu_notify(CPU_UP_CANCELED | mod, hcpu, nr_calls, NULL);
 out:
 	cpu_hotplug_done();
+	trace_sched_cpu_hotplug(cpu, ret, 1);
 
 	return ret;
 }
