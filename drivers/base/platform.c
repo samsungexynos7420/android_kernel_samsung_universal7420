1cc278a8c3eb49f5ed8f345ef150ab4490c7f448
Randy Dunlap <rdunlap@infradead.org>
2013-07-14 17:43:06 -0700

    driver-core: fix new kernel-doc warning in base/platform.c
    Fix new kernel-doc warning in drivers/base/platform.c:
    
    Warning(drivers/base/platform.c:528): No description found for parameter 'owner'
    
    Signed-off-by: Randy Dunlap <rdunlap@infradead.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/base/platform.c
+++ drivers/base/platform.c
@@ -521,6 +521,7 @@ static void platform_drv_shutdown(struct device *_dev)
 /**
  * platform_driver_register - register a driver for platform-level devices
  * @drv: platform driver structure
+ * @owner: owning module/driver
  */
 int platform_driver_register(struct platform_driver *drv)
 {
