ace2526928000d55f71f4e10f478d2ae2df629a8
Paul Moore <pmoore@redhat.com>
2013-07-23 17:38:40 -0400

    selinux: remove the BUG_ON() from selinux_skb_xfrm_sid()
    Remove the BUG_ON() from selinux_skb_xfrm_sid() and propogate the
    error code up to the caller.  Also check the return values in the
    only caller function, selinux_skb_peerlbl_sid().
    
    Signed-off-by: Paul Moore <pmoore@redhat.com>
    Signed-off-by: Eric Paris <eparis@redhat.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -4535,8 +4535,12 @@ static int selinux_skb_peerlbl_sid(struct sk_buff *skb, u16 family, u32 *sid)
 	u32 nlbl_sid;
 	u32 nlbl_type;
 
-	selinux_xfrm_skb_sid(skb, &xfrm_sid);
-	selinux_netlbl_skbuff_getsid(skb, family, &nlbl_type, &nlbl_sid);
+	err = selinux_xfrm_skb_sid(skb, &xfrm_sid);
+	if (unlikely(err))
+		return -EACCES;
+	err = selinux_netlbl_skbuff_getsid(skb, family, &nlbl_type, &nlbl_sid);
+	if (unlikely(err))
+		return -EACCES;
 
 	err = security_net_peersid_resolve(nlbl_sid, nlbl_type, xfrm_sid, sid);
 	if (unlikely(err)) {
