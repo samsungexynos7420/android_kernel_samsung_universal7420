4bd44f6b0d9187b23a169e9cfee4aa2a5cd7ad00
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2013-08-08 22:26:23 -0700

    rcu: Flag lockless access to ->gp_flags with ACCESS_ONCE()
    This commit applies ACCESS_ONCE() to an outside-of-lock access to
    ->gp_flags.  Although it is hard to imagine any sane compiler messing
    this particular case up, the documentation benefits are substantial.
    Plus the definition of "sane compiler" grows ever looser.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>

--- kernel/rcutree.c
+++ kernel/rcutree.c
@@ -1555,7 +1555,7 @@ static int __noreturn rcu_gp_kthread(void *arg)
 					       ACCESS_ONCE(rsp->gpnum),
 					       TPS("reqwait"));
 			wait_event_interruptible(rsp->gp_wq,
-						 rsp->gp_flags &
+						 ACCESS_ONCE(rsp->gp_flags) &
 						 RCU_GP_FLAG_INIT);
 			if (rcu_gp_init(rsp))
 				break;
