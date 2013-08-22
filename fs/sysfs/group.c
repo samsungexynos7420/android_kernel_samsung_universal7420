0994c313877a68b7e461f92cb15c119aff437a5b
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2013-08-22 09:23:28 -0700

    sysfs: group.c: fix up kerneldoc
    Fix up the wording of sysfs_create/remove_groups() a bit.
    
    Reported-by: Anthony Foiani <tkil@scrye.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- fs/sysfs/group.c
+++ fs/sysfs/group.c
@@ -115,7 +115,7 @@ int sysfs_create_group(struct kobject *kobj,
  * It will explicitly warn and error if any of the attribute files being
  * created already exist.
  *
- * Returns 0 on success or error code from sysfs_create_groups on error.
+ * Returns 0 on success or error code from sysfs_create_group on error.
  */
 int sysfs_create_groups(struct kobject *kobj,
 			const struct attribute_group **groups)
@@ -192,7 +192,7 @@ void sysfs_remove_group(struct kobject * kobj,
  * kobj:	The kobject for the groups to be removed from
  * groups:	NULL terminated list of groups to be removed
  *
- * If groups is not NULL, the all groups will be removed from the kobject
+ * If groups is not NULL, remove the specified groups from the kobject.
  */
 void sysfs_remove_groups(struct kobject *kobj,
 			 const struct attribute_group **groups)
