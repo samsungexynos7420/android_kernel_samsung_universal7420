0176f5cbce5ca7e4af90d0cc474ed184aaef27f6
Colin Cross <ccross@android.com>
2013-12-20 16:43:50 -0800

    dma-buf: avoid using IS_ERR_OR_NULL
    dma_buf_map_attachment and dma_buf_vmap can return NULL or
    ERR_PTR on a error.  This encourages a common buggy pattern in
    callers:
    	sgt = dma_buf_map_attachment(attach, DMA_BIDIRECTIONAL);
    	if (IS_ERR_OR_NULL(sgt))
                    return PTR_ERR(sgt);
    
    This causes the caller to return 0 on an error.  IS_ERR_OR_NULL
    is almost always a sign of poorly-defined error handling.
    
    This patch converts dma_buf_map_attachment to always return
    ERR_PTR, and fixes the callers that incorrectly handled NULL.
    There are a few more callers that were not checking for NULL
    at all, which would have dereferenced a NULL pointer later.
    There are also a few more callers that correctly handled NULL
    and ERR_PTR differently, I left those alone but they could also
    be modified to delete the NULL check.
    
    This patch also converts dma_buf_vmap to always return NULL.
    All the callers to dma_buf_vmap only check for NULL, and would
    have dereferenced an ERR_PTR and panic'd if one was ever
    returned. This is not consistent with the rest of the dma buf
    APIs, but matches the expectations of all of the callers.
    
    Signed-off-by: Colin Cross <ccross@android.com>
    Reviewed-by: Rob Clark <robdclark@gmail.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/media/v4l2-core/videobuf2-dma-contig.c
+++ drivers/media/v4l2-core/videobuf2-dma-contig.c
@@ -644,7 +644,7 @@ static int vb2_dc_map_dmabuf(void *mem_priv, int plane)
 
 	/* get the associated scatterlist for this buffer */
 	sgt = dma_buf_map_attachment(buf->db_attach, buf->dma_dir);
-	if (IS_ERR_OR_NULL(sgt)) {
+	if (IS_ERR(sgt)) {
 		pr_err("Error getting dmabuf scatterlist\n");
 		return -EINVAL;
 	}
