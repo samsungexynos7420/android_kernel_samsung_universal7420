0f4a51607e269fb62c06cadb41dcadad95412a02
Heiko Carstens <heiko.carstens@de.ibm.com>
2014-01-16 14:54:50 +0100

    futexes: Fix futex_hashsize initialization
    "futexes: Increase hash table size for better performance"
    introduces a new alloc_large_system_hash() call.
    
    alloc_large_system_hash() however may allocate less memory than
    requested, e.g. limited by MAX_ORDER.
    
    Hence pass a pointer to alloc_large_system_hash() which will
    contain the hash shift when the function returns. Afterwards
    correctly set futex_hashsize.
    
    Fixes a crash on s390 where the requested allocation size was
    4MB but only 1MB was allocated.
    
    Signed-off-by: Heiko Carstens <heiko.carstens@de.ibm.com>
    Cc: Darren Hart <dvhart@linux.intel.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Cc: Waiman Long <Waiman.Long@hp.com>
    Cc: Jason Low <jason.low2@hp.com>
    Cc: Davidlohr Bueso <davidlohr@hp.com>
    Link: http://lkml.kernel.org/r/20140116135450.GA4345@osiris
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- kernel/futex.c
+++ kernel/futex.c
@@ -3141,6 +3141,7 @@ static void __init futex_detect_cmpxchg(void)
 
 static int __init futex_init(void)
 {
+	unsigned int futex_shift;
 	unsigned long i;
 
 #if CONFIG_BASE_SMALL
@@ -3152,7 +3153,9 @@ static int __init futex_init(void)
 	futex_queues = alloc_large_system_hash("futex", sizeof(*futex_queues),
 					       futex_hashsize, 0,
 					       futex_hashsize < 256 ? HASH_SMALL : 0,
-					       NULL, NULL, futex_hashsize, futex_hashsize);
+					       &futex_shift, NULL,
+					       futex_hashsize, futex_hashsize);
+	futex_hashsize = 1UL << futex_shift;
 
 	futex_detect_cmpxchg();
 
