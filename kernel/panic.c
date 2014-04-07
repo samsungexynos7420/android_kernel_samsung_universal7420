52bb90816a0c329aea50d74157f7221dcd83aedd
Fabian Frederick <fabf@skynet.be>
2014-04-07 15:39:03 -0700

    kernel/panic.c: display reason at end + pr_emerg
    Currently, booting without initrd specified on 80x25 screen gives a call
    trace followed by atkbd : Spurious ACK.  Original message ("VFS: Unable
    to mount root fs") is not available.  Of course this could happen in
    other situations...
    
    This patch displays panic reason after call trace which could help lot
    of people even if it's not the very last line on screen.
    
    Also, convert all panic.c printk(KERN_EMERG to pr_emerg(
    
    [akpm@linux-foundation.org: missed a couple of pr_ conversions]
    Signed-off-by: Fabian Frederick <fabf@skynet.be>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- kernel/panic.c
+++ kernel/panic.c
@@ -120,7 +120,7 @@ void panic(const char *fmt, ...)
 	va_start(args, fmt);
 	vsnprintf(buf, sizeof(buf), fmt, args);
 	va_end(args);
-	printk(KERN_EMERG "Kernel panic - not syncing: %s\n",buf);
+	pr_emerg("Kernel panic - not syncing: %s\n",buf);
 
 	exynos_ss_prepare_panic();
 	exynos_ss_dump_panic();
@@ -174,7 +174,7 @@ void panic(const char *fmt, ...)
 		 * Delay timeout seconds before rebooting the machine.
 		 * We can't use the "normal" timers since we just panicked.
 		 */
-		printk(KERN_EMERG "Rebooting in %d seconds..", panic_timeout);
+		pr_emerg("Rebooting in %d seconds..", panic_timeout);
 
 		for (i = 0; i < panic_timeout * 1000; i += PANIC_TIMER_STEP) {
 			touch_nmi_watchdog();
@@ -198,7 +198,7 @@ void panic(const char *fmt, ...)
 		extern int stop_a_enabled;
 		/* Make sure the user can actually press Stop-A (L1-A) */
 		stop_a_enabled = 1;
-		printk(KERN_EMERG "Press Stop-A (L1-A) to return to the boot prom\n");
+		pr_emerg("Press Stop-A (L1-A) to return to the boot prom\n");
 	}
 #endif
 #if defined(CONFIG_S390)
@@ -209,6 +209,7 @@ void panic(const char *fmt, ...)
 		disabled_wait(caller);
 	}
 #endif
+	pr_emerg("---[ end Kernel panic - not syncing: %s\n", buf);
 	local_irq_enable();
 	for (i = 0; ; i += PANIC_TIMER_STEP) {
 		touch_softlockup_watchdog();
@@ -307,8 +308,7 @@ unsigned long get_taint(void)
 void add_taint(unsigned flag, enum lockdep_ok lockdep_ok)
 {
 	if (lockdep_ok == LOCKDEP_NOW_UNRELIABLE && __debug_locks_off())
-		printk(KERN_WARNING
-		       "Disabling lock debugging due to kernel taint\n");
+		pr_warn("Disabling lock debugging due to kernel taint\n");
 
 	set_bit(flag, &tainted_mask);
 }
@@ -415,10 +415,10 @@ void print_oops_end_marker(void)
 	init_oops_id();
 
 	if (mach_panic_string)
-		printk(KERN_WARNING "Board Information: %s\n",
+		pr_warn("Board Information: %s\n",
 		       mach_panic_string);
 
-	printk(KERN_WARNING "---[ end trace %016llx ]---\n",
+	pr_warn("---[ end trace %016llx ]---\n",
 		(unsigned long long)oops_id);
 }
 
