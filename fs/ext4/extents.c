c013d441e3c0112e6b0b01f14428187e4fc67cdd
Lukas Czerner <lczerner@redhat.com>
2014-04-18 10:48:25 -0400

    ext4: no need to truncate pagecache twice in collapse range
    We're already calling truncate_pagecache() before we attempt to do any
    actual job so there is not need to truncate pagecache once more using
    truncate_setsize() after we're finished.
    
    Remove truncate_setsize() and replace it just with i_size_write() note
    that we're holding appropriate locks.
    
    Signed-off-by: Lukas Czerner <lczerner@redhat.com>
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>

--- fs/ext4/extents.c
+++ fs/ext4/extents.c
@@ -5268,7 +5268,7 @@ int ext4_collapse_range(struct inode *inode, loff_t offset, loff_t len)
 	}
 
 	new_size = i_size_read(inode) - len;
-	truncate_setsize(inode, new_size);
+	i_size_write(inode, new_size);
 	EXT4_I(inode)->i_disksize = new_size;
 
 	up_write(&EXT4_I(inode)->i_data_sem);
