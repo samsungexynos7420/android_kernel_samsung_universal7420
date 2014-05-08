61deaf9b1832700c730c1f07f66ba26c7ef9c2a5
Dongsheng Yang <yangds.fnst@cn.fujitsu.com>
2014-05-08 18:33:49 +0900

    sched: Consolidate open coded implementations of nice level frobbing into nice_to_rlimit() and rlimit_to_nice()
    Signed-off-by: Dongsheng Yang <yangds.fnst@cn.fujitsu.com>
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/a568a1e3cc8e78648f41b5035fa5e381d36274da.1399532322.git.yangds.fnst@cn.fujitsu.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -1177,7 +1177,7 @@ static void binder_do_set_priority(struct task_struct *task,
 	}
 
 	if (verify && is_fair_policy(policy) && !has_cap_nice) {
-		long min_nice = (MAX_NICE - task_rlimit(task, RLIMIT_NICE) + 1);
+		long min_nice = rlimit_to_nice(task_rlimit(task, RLIMIT_NICE));
 
 		if (min_nice > MAX_NICE) {
 			binder_user_error("%d RLIMIT_NICE not set\n",
