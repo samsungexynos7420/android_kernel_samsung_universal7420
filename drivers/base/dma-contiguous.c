2ac657d11474efd31f097079fd487280ad5868ed
Gioh Kim <gioh.kim@lge.com>
2014-05-22 13:31:37 +0900

    drivers/base/dma-contiguous.c: erratum of dev_get_cma_area
    fix erratum get_dev_cma_area into dev_get_cma_area
    
    Signed-off-by: Gioh Kim <gioh.kim@lge.com>
    Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>

--- drivers/base/dma-contiguous.c
+++ drivers/base/dma-contiguous.c
@@ -333,7 +333,7 @@ int __init dma_contiguous_reserve_area(phys_addr_t size, phys_addr_t base,
  *
  * This function allocates memory buffer for specified device. It uses
  * device specific contiguous memory area if available or the default
- * global one. Requires architecture specific get_dev_cma_area() helper
+ * global one. Requires architecture specific dev_get_cma_area() helper
  * function.
  */
 struct page *dma_alloc_from_contiguous(struct device *dev, int count,
