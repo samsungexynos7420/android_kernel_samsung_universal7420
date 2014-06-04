0f89ff015ba1543421c8dfdbf58d848185084ccd
Heinrich Schuchardt <xypron.glpk@gmx.de>
2014-06-04 16:05:40 -0700

    fanotify: FAN_MARK_FLUSH: avoid having to provide a fake/invalid fd and path
    Originally from Tvrtko Ursulin (https://lkml.org/lkml/2011/1/12/112)
    
    Avoid having to provide a fake/invalid fd and path when flushing marks
    
    Currently for a group to flush marks it has set it needs to provide a
    fake or invalid (but resolvable) file descriptor and path when calling
    fanotify_mark.  This patch pulls the flush handling a bit up so file
    descriptor and path are completely ignored when flushing.
    
    I reworked the patch to be applicable again (the signature of
    fanotify_mark has changed since Tvrtko's work).
    
    Signed-off-by: Heinrich Schuchardt <xypron.glpk@gmx.de>
    Cc: Tvrtko Ursulin <tvrtko.ursulin@onelan.co.uk>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Acked-by: Eric Paris <eparis@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- fs/notify/fanotify/fanotify_user.c
+++ fs/notify/fanotify/fanotify_user.c
@@ -792,6 +792,15 @@ SYSCALL_DEFINE5(fanotify_mark, int, fanotify_fd, unsigned int, flags,
 	    group->priority == FS_PRIO_0)
 		goto fput_and_out;
 
+	if (flags & FAN_MARK_FLUSH) {
+		ret = 0;
+		if (flags & FAN_MARK_MOUNT)
+			fsnotify_clear_vfsmount_marks_by_group(group);
+		else
+			fsnotify_clear_inode_marks_by_group(group);
+		goto fput_and_out;
+	}
+
 	ret = fanotify_find_path(dfd, pathname, &path, flags);
 	if (ret)
 		goto fput_and_out;
@@ -803,7 +812,7 @@ SYSCALL_DEFINE5(fanotify_mark, int, fanotify_fd, unsigned int, flags,
 		mnt = path.mnt;
 
 	/* create/update an inode mark */
-	switch (flags & (FAN_MARK_ADD | FAN_MARK_REMOVE | FAN_MARK_FLUSH)) {
+	switch (flags & (FAN_MARK_ADD | FAN_MARK_REMOVE)) {
 	case FAN_MARK_ADD:
 		if (flags & FAN_MARK_MOUNT)
 			ret = fanotify_add_vfsmount_mark(group, mnt, mask, flags);
@@ -815,12 +824,6 @@ SYSCALL_DEFINE5(fanotify_mark, int, fanotify_fd, unsigned int, flags,
 			ret = fanotify_remove_vfsmount_mark(group, mnt, mask, flags);
 		else
 			ret = fanotify_remove_inode_mark(group, inode, mask, flags);
-		break;
-	case FAN_MARK_FLUSH:
-		if (flags & FAN_MARK_MOUNT)
-			fsnotify_clear_vfsmount_marks_by_group(group);
-		else
-			fsnotify_clear_inode_marks_by_group(group);
 		break;
 	default:
 		ret = -EINVAL;
