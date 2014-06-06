6f2c06480704997e94ee8261573fecd56d55d6ee
Octavian Purdila <octavian.purdila@intel.com>
2014-06-06 17:32:37 +0300

    tcp: add gfp parameter to tcp_fragment
    tcp_fragment can be called from process context (from tso_fragment).
    Add a new gfp parameter to allow it to preserve atomic memory if
    possible.
    
    Signed-off-by: Octavian Purdila <octavian.purdila@intel.com>
    Reviewed-by: Christoph Paasch <christoph.paasch@uclouvain.be>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -1235,11 +1235,7 @@ static int tcp_match_skb_to_sack(struct sock *sk, struct sk_buff *skb,
 		if (pkt_len >= skb->len && !in_sack)
 			return 0;
 
-#ifdef CONFIG_MPTCP
 		err = tcp_fragment(sk, skb, pkt_len, mss, GFP_ATOMIC);
-#else
-		err = tcp_fragment(sk, skb, pkt_len, mss);
-#endif
 		if (err < 0)
 			return err;
 	}
@@ -2333,11 +2329,7 @@ static void tcp_mark_head_lost(struct sock *sk, int packets, int mark_head)
 			/* If needed, chop off the prefix to mark as lost. */
 			lost = (packets - oldcnt) * mss;
 			if (lost < skb->len &&
-#ifdef CONFIG_MPTCP			
 			    tcp_fragment(sk, skb, lost, mss, GFP_ATOMIC) < 0)
-#else
-				tcp_fragment(sk, skb, lost, mss) < 0)
-#endif
 				break;
 			cnt = packets;
 		}
