163f387cacd1e2fda268b612f6c6d8c2cfd8d21c
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2014-08-08 12:51:39 +0100

    arm64: add helper functions to read I-cache attributes
    This adds helper functions and #defines to <asm/cachetype.h> to read the
    line size and the number of sets from the level 1 instruction cache.
    
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Acked-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    
    fakeman: with an include for 3.10

--- arch/arm64/kernel/cpuinfo.c
+++ arch/arm64/kernel/cpuinfo.c
@@ -21,9 +21,12 @@
 #include <asm/cpufeature.h>
 
 #include <linux/bitops.h>
+#include <linux/bug.h>
+#include <linux/hardirq.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/personality.h>
+#include <linux/preempt.h>
 #include <linux/printk.h>
 #include <linux/seq_file.h>
 #include <linux/sched.h>
@@ -234,4 +237,16 @@ void __init cpuinfo_store_boot_cpu(void)
 
 	boot_cpu_data = *info;
 	init_cpu_features(&boot_cpu_data);
+}
+
+u64 __attribute_const__ icache_get_ccsidr(void)
+{
+	u64 ccsidr;
+
+	WARN_ON(preemptible());
+
+	/* Select L1 I-cache and read its size ID register */
+	asm("msr csselr_el1, %1; isb; mrs %0, ccsidr_el1"
+	    : "=r"(ccsidr) : "r"(1L));
+	return ccsidr;
 }
