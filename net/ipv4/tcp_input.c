82ecdebda5b1e26caf1200c1c4c56bbaca6f784c
Peter Pan(潘卫平) <panweiping3@gmail.com>
2014-09-24 22:17:02 +0800

    tcp: use tcp_flags in tcp_data_queue()
    This patch is a cleanup which follows the idea in commit e11ecddf5128 (tcp: use
    TCP_SKB_CB(skb)->tcp_flags in input path),
    and it may reduce register pressure since skb->cb[] access is fast,
    bacause skb is probably in a register.
    
    v2: remove variable th
    v3: reword the changelog
    
    Signed-off-by: Weiping Pan <panweiping3@gmail.com>
    Acked-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -4551,7 +4551,6 @@ err:
 
 static void tcp_data_queue(struct sock *sk, struct sk_buff *skb)
 {
-	const struct tcphdr *th = tcp_hdr(skb);
 	struct tcp_sock *tp = tcp_sk(sk);
 	int eaten = -1;
 	bool fragstolen = false;
@@ -4566,7 +4565,7 @@ static void tcp_data_queue(struct sock *sk, struct sk_buff *skb)
 		goto drop;
 
 	skb_dst_drop(skb);
-	__skb_pull(skb, th->doff * 4);
+	__skb_pull(skb, tcp_hdr(skb)->doff * 4);
 
 	TCP_ECN_accept_cwr(tp, skb);
 
@@ -4614,7 +4613,7 @@ queue_and_out:
 #endif
 			)
 			tcp_event_data_recv(sk, skb);
-		if (th->fin)
+		if (TCP_SKB_CB(skb)->tcp_flags & TCPHDR_FIN)
 			tcp_fin(sk);
 
 		if (!skb_queue_empty(&tp->out_of_order_queue)) {
