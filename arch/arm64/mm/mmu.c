ec3d0528075e514aa79e3e7c644be02bf96081c2
Min-Hua Chen <orca.chen@gmail.com>
2014-10-08 16:45:59 +0100

    arm64: Fix data type for physical address
    Use phys_addr_t for physical address in alloc_init_pud. Although
    phys_addr_t and unsigned long are 64 bit in arm64, it is better
    to use phys_addr_t to describe physical addresses.
    
    Signed-off-by: Min-Hua Chen <orca.chen@gmail.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>

--- arch/arm64/mm/mmu.c
+++ arch/arm64/mm/mmu.c
@@ -310,7 +310,7 @@ static void __init alloc_init_pmd(pud_t *pud, unsigned long addr,
 }
 
 static void __init alloc_init_pud(pgd_t *pgd, unsigned long addr,
-				  unsigned long end, unsigned long phys)
+				  unsigned long end, phys_addr_t phys)
 {
 	pud_t *pud;
 	unsigned long next;
