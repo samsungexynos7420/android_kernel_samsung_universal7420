6fa26963e7d0bc4d52a0fa6253fcd1bbb9db0cad
Andre Przywara <andre.przywara@arm.com>
2014-11-14 15:54:08 +0000

    UPSTREAM: arm64: add alternative runtime patching
    With a blatant copy of some x86 bits we introduce the alternative
    runtime patching "framework" to arm64.
    This is quite basic for now and we only provide the functions we need
    at this time.
    This is connected to the newly introduced feature bits.
    
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    
    Bug: 31432001
    Change-Id: I2420b40a45a2206b05415fdcdf72ef210cba25a3
    (cherry picked from commit e039ee4ee3fcf174736f2cb0a2eed6cb908348a6)
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>

--- arch/arm64/mm/init.c
+++ arch/arm64/mm/init.c
@@ -38,6 +38,7 @@
 #include <asm/setup.h>
 #include <asm/sizes.h>
 #include <asm/tlb.h>
+#include <asm/alternative.h>
 
 #include "mm.h"
 
@@ -394,6 +395,7 @@ void free_initmem(void)
 {
 	poison_init_mem(__init_begin, __init_end - __init_begin);
 	free_initmem_default(0);
+	free_alternatives_memory();
 #ifdef CONFIG_TIMA_RKP
 #ifdef CONFIG_KNOX_KAP
 	if (boot_mode_security)
