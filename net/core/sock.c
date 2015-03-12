047fbf03924cadfcc2ef32769abac41e5a9e5d5f
Eric Dumazet <edumazet@google.com>
2015-03-11 18:53:14 -0700

    net: add real socket cookies
    A long standing problem in netlink socket dumps is the use
    of kernel socket addresses as cookies.
    
    1) It is a security concern.
    
    2) Sockets can be reused quite quickly, so there is
       no guarantee a cookie is used once and identify
       a flow.
    
    3) request sock, establish sock, and timewait socks
       for a given flow have different cookies.
    
    Part of our effort to bring better TCP statistics requires
    to switch to a different allocator.
    
    In this patch, I chose to use a per network namespace 64bit generator,
    and to use it only in the case a socket needs to be dumped to netlink.
    (This might be refined later if needed)
    
    Note that I tried to carry cookies from request sock, to establish sock,
    then timewait sockets.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Cc: Eric Salo <salo@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Change-Id: I08ce6f65ea82da6b166644715d6a37f62ee62ac5

--- net/core/sock.c
+++ net/core/sock.c
@@ -1612,6 +1612,7 @@ struct sock *sk_clone_lock(const struct sock *sk, const gfp_t priority)
 
 		cgroup_sk_alloc(&newsk->sk_cgrp_data);
 
+		atomic64_set(&newsk->sk_cookie, 0);
 		/*
 		 * Before updating sk_refcnt, we must commit prior changes to memory
 		 * (Documentation/RCU/rculist_nulls.txt for details)
