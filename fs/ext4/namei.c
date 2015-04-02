e52df2098c83d38e6f9c02cb878fb5f984605e23
Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
2015-04-02 16:32:15 -0400

    ext4: fix bh leak on error paths in ext4_rename()
    [ Upstream commit 7071b715873a66b69a9c0c5839963bb51aeae41b ]
    
    Release references to buffer-heads if ext4_journal_start() fails.
    
    Fixes: 5b61de757535 ("ext4: start handle at least possible moment when renaming files")
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Sasha Levin <sasha.levin@oracle.com>
    Signed-off-by: TALU <taluemail272@gmail.com>

--- fs/ext4/namei.c
+++ fs/ext4/namei.c
@@ -3351,8 +3351,11 @@ static int ext4_rename(struct inode *old_dir, struct dentry *old_dentry,
 	handle = ext4_journal_start(old.dir, EXT4_HT_DIR,
 		(2 * EXT4_DATA_TRANS_BLOCKS(old.dir->i_sb) +
 		 EXT4_INDEX_EXTRA_TRANS_BLOCKS + 2));
-	if (IS_ERR(handle))
-		return PTR_ERR(handle);
+	if (IS_ERR(handle)) {
+		retval = PTR_ERR(handle);
+		handle = NULL;
+		goto end_rename;
+	}
 
 	if (IS_DIRSYNC(old.dir) || IS_DIRSYNC(new.dir))
 		ext4_handle_sync(handle);
