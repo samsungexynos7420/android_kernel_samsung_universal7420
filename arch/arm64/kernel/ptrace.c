f4d9faeae9b6f0422f577099257ceadb7765a0d1
Daniel Kurtz <djkurtz@google.com>
2015-04-22 13:25:48 +0800

    Revert "arm64: add seccomp support"
    This reverts commit 923b5e258a9a30ccf8ce133c63f15b21da2182d6.
    [djkurtz: trivial conflict fixup in arch/arm64/Kconfig]
    
    To be replaced with arm64 seccomp from mainline v3.19.
    
    Change-Id: Icad82d42b1b3d755c8130657cc3675d460320b86
    Signed-off-by: Daniel Kurtz <djkurtz@google.com>

--- arch/arm64/kernel/ptrace.c
+++ arch/arm64/kernel/ptrace.c
@@ -27,7 +27,6 @@
 #include <linux/smp.h>
 #include <linux/ptrace.h>
 #include <linux/user.h>
-#include <linux/seccomp.h>
 #include <linux/security.h>
 #include <linux/init.h>
 #include <linux/signal.h>
