82efdcb20abf7504ebfc91fb46394bcdea08c937
Andre Przywara <andre.przywara@arm.com>
2015-04-24 10:30:49 -0700

    arm64: add Cortex-A57 erratum 832075 workaround
    The ARM erratum 832075 applies to certain revisions of Cortex-A57,
    one of the workarounds is to change device loads into using
    load-aquire semantics.
    This is achieved using the alternatives framework.
    
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    
    Cc: <stable@vger.kernel.org> # v3.18.y
    (cherry picked from commit 5afaa1fc1b320cec48affa7e6949f2493f875c12)
    Signed-off-by: Kevin Hilman <khilman@linaro.org>
    Signed-off-by: Sasha Levin <sasha.levin@oracle.com>

--- arch/arm64/include/asm/io.h
+++ arch/arm64/include/asm/io.h
@@ -28,6 +28,8 @@
 #include <asm/barrier.h>
 #include <asm/pgtable.h>
 #include <asm/early_ioremap.h>
+#include <asm/alternative.h>
+#include <asm/cpufeature.h>
 
 /*
  * Generic IO read/write.  These perform native-endian accesses.
@@ -64,7 +66,10 @@ static inline u8 __raw_readb(const volatile void __iomem *addr)
 {
 	u8 val;
 	exynos_ss_reg(1, 0, (size_t)addr, ESS_FLAG_IN);
-	asm volatile("ldrb %w0, [%1]" : "=r" (val) : "r" (addr));
+	asm volatile(ALTERNATIVE("ldrb %w0, [%1]",
+				 "ldarb %w0, [%1]",
+				 ARM64_WORKAROUND_DEVICE_LOAD_ACQUIRE)
+		     : "=r" (val) : "r" (addr));
 	exynos_ss_reg(1, (size_t)val, (size_t)addr, ESS_FLAG_OUT);
 	return val;
 }
@@ -73,7 +78,10 @@ static inline u16 __raw_readw(const volatile void __iomem *addr)
 {
 	u16 val;
 	exynos_ss_reg(1, 0, (size_t)addr, ESS_FLAG_IN);
-	asm volatile("ldrh %w0, [%1]" : "=r" (val) : "r" (addr));
+	asm volatile(ALTERNATIVE("ldrh %w0, [%1]",
+				 "ldarh %w0, [%1]",
+				 ARM64_WORKAROUND_DEVICE_LOAD_ACQUIRE)
+		     : "=r" (val) : "r" (addr));
 	exynos_ss_reg(1, (size_t)val, (size_t)addr, ESS_FLAG_OUT);
 	return val;
 }
@@ -82,7 +90,10 @@ static inline u32 __raw_readl(const volatile void __iomem *addr)
 {
 	u32 val;
 	exynos_ss_reg(1, 0, (size_t)addr, ESS_FLAG_IN);
-	asm volatile("ldr %w0, [%1]" : "=r" (val) : "r" (addr));
+	asm volatile(ALTERNATIVE("ldr %w0, [%1]",
+				 "ldar %w0, [%1]",
+				 ARM64_WORKAROUND_DEVICE_LOAD_ACQUIRE)
+		     : "=r" (val) : "r" (addr));
 	exynos_ss_reg(1, (size_t)val, (size_t)addr, ESS_FLAG_OUT);
 	return val;
 }
@@ -91,7 +102,10 @@ static inline u64 __raw_readq(const volatile void __iomem *addr)
 {
 	u64 val;
 	exynos_ss_reg(1, 0, (size_t)addr, ESS_FLAG_IN);
-	asm volatile("ldr %0, [%1]" : "=r" (val) : "r" (addr));
+	asm volatile(ALTERNATIVE("ldr %0, [%1]",
+				 "ldar %0, [%1]",
+				 ARM64_WORKAROUND_DEVICE_LOAD_ACQUIRE)
+		     : "=r" (val) : "r" (addr));
 	exynos_ss_reg(1, (size_t)val, (size_t)addr, ESS_FLAG_OUT);
 	return val;
 }
