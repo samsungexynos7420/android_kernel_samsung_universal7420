d6339bc262b4924f455fd8e1c9a2ff1636a02c42
Daniel Borkmann <daniel@iogearbox.net>
2015-04-30 16:17:27 +0200

    test_bpf: indicate whether bpf prog got jited in test suite
    I think this is useful to verify whether a filter could be JITed or
    not in case of bpf_prog_enable >= 1, which otherwise the test suite
    doesn't tell besides taking a good peek at the performance numbers.
    
    Nicolas Schichan reported a bug in the ARM JIT compiler that rejected
    and waved the filter to the interpreter although it shouldn't have.
    Nevertheless, the test passes as expected, but such information is
    not visible.
    
    It's i.e. useful for the remaining classic JITs, but also for
    implementing remaining opcodes that are not yet present in eBPF JITs
    (e.g. ARM64 waves some of them to the interpreter). This minor patch
    allows to grep through dmesg to find those accordingly, but also
    provides a total summary, i.e.: [<X>/53 JIT'ed]
    
      # echo 1 > /proc/sys/net/core/bpf_jit_enable
      # insmod lib/test_bpf.ko
      # dmesg | grep "jited:0"
    
    dmesg example on the ARM issue with JIT rejection:
    
    [...]
    [   67.925387] test_bpf: #2 ADD_SUB_MUL_K jited:1 24 PASS
    [   67.930889] test_bpf: #3 DIV_MOD_KX jited:0 794 PASS
    [   67.943940] test_bpf: #4 AND_OR_LSH_K jited:1 20 20 PASS
    [...]
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Cc: Nicolas Schichan <nschichan@freebox.fr>
    Cc: Alexei Starovoitov <ast@plumgrid.com>
    Acked-by: Alexei Starovoitov <ast@plumgrid.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- lib/test_bpf.c
+++ lib/test_bpf.c
@@ -1944,6 +1944,7 @@ static int run_one(const struct bpf_prog *fp, struct bpf_test *test)
 static __init int test_bpf(void)
 {
 	int i, err_cnt = 0, pass_cnt = 0;
+	int jit_cnt = 0, run_cnt = 0;
 
 	for (i = 0; i < ARRAY_SIZE(tests); i++) {
 		struct bpf_prog *fp;
@@ -1960,6 +1961,13 @@ static __init int test_bpf(void)
 
 			return err;
 		}
+
+		pr_cont("jited:%u ", fp->jited);
+
+		run_cnt++;
+		if (fp->jited)
+			jit_cnt++;
+
 		err = run_one(fp, &tests[i]);
 		release_filter(fp, i);
 
@@ -1972,7 +1980,9 @@ static __init int test_bpf(void)
 		}
 	}
 
-	pr_info("Summary: %d PASSED, %d FAILED\n", pass_cnt, err_cnt);
+	pr_info("Summary: %d PASSED, %d FAILED, [%d/%d JIT'ed]\n",
+		pass_cnt, err_cnt, jit_cnt, run_cnt);
+
 	return err_cnt ? -EINVAL : 0;
 }
 
