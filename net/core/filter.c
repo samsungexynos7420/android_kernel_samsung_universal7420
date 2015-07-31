e693c672631c021abddeb0121d40c4435befbef2
Daniel Borkmann <daniel@iogearbox.net>
2015-08-01 00:46:29 +0200

    ebpf: add skb->hash to offset map for usage in {cls, act}_bpf or filters
    Add skb->hash to the __sk_buff offset map, so it can be accessed from
    an eBPF program. We currently already do this for classic BPF filters,
    but not yet on eBPF, it might be useful as a demuxer in combination with
    helpers like bpf_clone_redirect(), toy example:
    
      __section("cls-lb") int ingress_main(struct __sk_buff *skb)
      {
        unsigned int which = 3 + (skb->hash & 7);
        /* bpf_skb_store_bytes(skb, ...); */
        /* bpf_l{3,4}_csum_replace(skb, ...); */
        bpf_clone_redirect(skb, which, 0);
        return -1;
      }
    
    I was thinking whether to add skb_get_hash(), but then concluded the
    raw skb->hash seems fine in this case: we can directly access the hash
    w/o extra eBPF helper function call, it's filled out by many NICs on
    ingress, and in case the entropy level would not be sufficient, people
    can still implement their own specific sw fallback hash mix anyway.
    
    Change-Id: I73199d96bfbb0ebb907643e1b4feeb0ab5aa64cd
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Alexei Starovoitov <ast@plumgrid.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/core/filter.c
+++ net/core/filter.c
@@ -1565,6 +1565,13 @@ static u32 bpf_net_convert_ctx_access(enum bpf_access_type type, int dst_reg,
 				      offsetof(struct net_device, ifindex));
 		break;
 
+	case offsetof(struct __sk_buff, hash):
+		BUILD_BUG_ON(FIELD_SIZEOF(struct sk_buff, rxhash) != 4);
+
+		*insn++ = BPF_LDX_MEM(BPF_W, dst_reg, src_reg,
+				      offsetof(struct sk_buff, rxhash));
+		break;
+
 	case offsetof(struct __sk_buff, mark):
 		BUILD_BUG_ON(FIELD_SIZEOF(struct sk_buff, mark) != 4);
 
