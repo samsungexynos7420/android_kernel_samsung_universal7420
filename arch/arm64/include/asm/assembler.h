48f00eb49ee3dcfe51bfcf30abc23c236e0dc761
Ard Biesheuvel <ard.biesheuvel@linaro.org>
2015-10-08 20:02:03 +0100

    BACKPORT: arm64: use ENDPIPROC() to annotate position independent assembler routines
    For more control over which functions are called with the MMU off or
    with the UEFI 1:1 mapping active, annotate some assembler routines as
    position independent. This is done by introducing ENDPIPROC(), which
    replaces the ENDPROC() declaration of those routines.
    
    Signed-off-by: Ard Biesheuvel <ard.biesheuvel@linaro.org>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    
    Bug: 31432001
    Change-Id: I757ad5a4e90100c8b56eff97a877e59463705db1
    (cherry picked from commit 207918461eb0aca720fddec5da79bc71c133b9f1)
    Signed-off-by: Sami Tolvanen <samitolvanen@google.com>

--- arch/arm64/include/asm/assembler.h
+++ arch/arm64/include/asm/assembler.h
@@ -287,4 +287,15 @@ lr	.req	x30		// link register
 9000:
 	.endm
 
+/*
+ * Annotate a function as position independent, i.e., safe to be called before
+ * the kernel virtual mapping is activated.
+ */
+#define ENDPIPROC(x)			\
+	.globl	__pi_##x;		\
+	.type 	__pi_##x, %function;	\
+	.set	__pi_##x, x;		\
+	.size	__pi_##x, . - x;	\
+	ENDPROC(x)
+
 #endif	/* __ASM_ASSEMBLER_H */
