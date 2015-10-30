8da6b00f138247ad0d24918cd59823e65f1ac976
Park Ju Hyung <qkrwngud825@gmail.com>
2015-10-31 01:52:24 +0900

    dm_cpu_hotplug: save more power
     - Properly disables hotplugging on screen-on state
     - Force to *enter* CMD_LOW_POWER(only cpu0 active) on screen-off state
     - Limit the available CPU cores to cpu0 and cpu1 on screen-off state
       On many Android devices, only cpu0 gets left on during screen-off
       state. Since 2 A53 cores can outperform A9 and A15, this makes sense.
     - Add helpful debugging messages with dm_dbg
    
    Change-Id: I0587fdbaf707aa134269612bcfca9ffcd5e01c8f
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- drivers/cpufreq/dm_cpu_hotplug.c
+++ drivers/cpufreq/dm_cpu_hotplug.c
@@ -29,6 +29,14 @@
 #include <linux/suspend.h>
 #include <linux/exynos-ss.h>
 
+// #define DM_HOTPLUG_DEBUG
+
+#ifdef DM_HOTPLUG_DEBUG
+#define dm_dbg(__dev, format, args...) pr_info(__dev, format, ##args)
+#else
+#define dm_dbg(__dev, format, args...) ((void)0)
+#endif
+
 #if defined(CONFIG_SOC_EXYNOS5430)
 #define NORMALMIN_FREQ	1000000
 #else
@@ -513,6 +521,9 @@ static void thread_manage_work(struct work_struct *work)
 		}
 
 		wake_up_process(dm_hotplug_task);
+
+		if (!dynamic_hotplug(CMD_LOW_POWER))
+			prev_cmd = CMD_LOW_POWER;
 	} else {
 		if (dm_hotplug_task) {
 			kthread_stop(dm_hotplug_task);
@@ -520,6 +531,7 @@ static void thread_manage_work(struct work_struct *work)
 			if (!dynamic_hotplug(CMD_NORMAL))
 				prev_cmd = CMD_NORMAL;
 		}
+		dm_hotplug_disable = 1;
 	}
 	mutex_unlock(&thread_manage_lock);
 }
@@ -554,6 +566,7 @@ static int fb_state_change(struct notifier_block *nb,
 		delay = POLLING_MSEC_DISP_OFF;
 
 #ifdef CONFIG_HOTPLUG_THREAD_STOP
+		dm_hotplug_disable = 0;
 		if (thread_manage_wq) {
 			if (work_pending(&manage_work))
 				flush_work(&manage_work);
@@ -603,8 +616,35 @@ static int __ref __cpu_hotplug(bool out_flag, enum hotplug_cmd cmd)
 {
 	int i = 0;
 	int ret = 0;
-#if defined(CONFIG_SCHED_HMP)
-	int hotplug_out_limit = 0;
+#ifdef DM_HOTPLUG_DEBUG
+	char cmddesc[25];
+
+	switch (cmd) {
+	case CMD_LOW_POWER:
+		strcpy(cmddesc, "CMD_LOW_POWER");
+		break;
+	case CMD_CLUST0_ONE_OUT:
+		strcpy(cmddesc, "CMD_CLUST0_ONE_OUT");
+		break;
+	case CMD_CLUST1_OUT:
+		strcpy(cmddesc, "CMD_CLUST1_OUT");
+		break;
+	case CMD_SLEEP_PREPARE:
+		strcpy(cmddesc, "CMD_SLEEP_PREPARE");
+		break;
+	case CMD_CLUST0_ONE_IN:
+		strcpy(cmddesc, "CMD_CLUST0_ONE_IN");
+		break;
+	case CMD_CLUST1_IN:
+		strcpy(cmddesc, "CMD_CLUST1_IN");
+		break;
+	case CMD_CLUST0_IN:
+		strcpy(cmddesc, "CMD_CLUST0_IN");
+		break;
+	case CMD_NORMAL:
+		strcpy(cmddesc, "CMD_NORMAL");
+		break;
+	}
 #endif
 	int tmp_nr_sleep_prepare_cpus = 0;
 
@@ -617,22 +657,16 @@ static int __ref __cpu_hotplug(bool out_flag, enum hotplug_cmd cmd)
 			goto blk_out;
 
 		if (cmd == CMD_SLEEP_PREPARE) {
-			if(fp_lockscreen_mode)
-				/* for finger-print boosting */
-				tmp_nr_sleep_prepare_cpus = nr_sleep_prepare_cpus + 1;
-			else
-				tmp_nr_sleep_prepare_cpus = nr_sleep_prepare_cpus;
-			printk(KERN_INFO "nr_sleep_prepare_cpus : %d, tmp_nr_sleep_prepare_cpus : %d\n", 
-				nr_sleep_prepare_cpus, tmp_nr_sleep_prepare_cpus);
-
-			for (i = setup_max_cpus - 1; i >= tmp_nr_sleep_prepare_cpus; i--) {
+			dm_dbg("%s: 1, %s\n", __func__, cmddesc);
+			for (i = setup_max_cpus - 1; i >= NR_CLUST0_CPUS; i--) {
                                 if (cpu_online(i)) {
                                         ret = cpu_down(i);
                                         if (ret)
                                                 goto blk_out;
                                 }
 			}
-			for (i = 1; i < tmp_nr_sleep_prepare_cpus; i++) {
+			dm_dbg("%s: 2, %s\n", __func__, cmddesc);
+			for (i = 1; i < nr_sleep_prepare_cpus; i++) {
 				if (!cpu_online(i)) {
 					ret = cpu_up(i);
 					if (ret)
@@ -641,6 +675,7 @@ static int __ref __cpu_hotplug(bool out_flag, enum hotplug_cmd cmd)
 			}
 		}
 		else if (cmd == CMD_CLUST1_OUT && !in_low_power_mode) {
+			dm_dbg("%s: 3, %s\n", __func__, cmddesc);
 			for (i = max_num_cpu - 1; i >= NR_CLUST0_CPUS; i--) {
 				if (cpu_online(i)) {
 					ret = cpu_down(i);
@@ -653,6 +688,7 @@ static int __ref __cpu_hotplug(bool out_flag, enum hotplug_cmd cmd)
 				if (!in_low_power_mode)
 					goto blk_out;
 
+				dm_dbg("%s: 4, %s\n", __func__, cmddesc);
 				for (i = NR_CLUST0_CPUS - 2; i > 0; i--) {
 					if (cpu_online(i)) {
 						ret = cpu_down(i);
@@ -661,10 +697,8 @@ static int __ref __cpu_hotplug(bool out_flag, enum hotplug_cmd cmd)
 					}
 				}
 			} else {
-				if (cluster0_hotplug_in)
-					hotplug_out_limit = NR_CLUST0_CPUS - 2;
-
-				for (i = max_num_cpu - 1; i > hotplug_out_limit; i--) {
+				dm_dbg("%s: 5, %s\n", __func__, cmddesc);
+				for (i = max_num_cpu - 1; i > 0; i--) {
 					if (cpu_online(i)) {
 						ret = cpu_down(i);
 						if (ret)
@@ -681,6 +715,7 @@ static int __ref __cpu_hotplug(bool out_flag, enum hotplug_cmd cmd)
 			if (in_low_power_mode)
 				goto blk_out;
 
+			dm_dbg("%s: 6, %s\n", __func__, cmddesc);
 			for (i = NR_CLUST0_CPUS; i < max_num_cpu; i++) {
 				if (!cpu_online(i)) {
 					ret = cpu_up(i);
@@ -690,24 +725,45 @@ static int __ref __cpu_hotplug(bool out_flag, enum hotplug_cmd cmd)
 			}
 		} else {
 			if (cmd == CMD_CLUST0_ONE_IN) {
-				for (i = 1; i < NR_CLUST0_CPUS - 1; i++) {
-					if (!cpu_online(i)) {
-						ret = cpu_up(i);
+				if (!lcd_is_on) {
+					dm_dbg("%s: 7, %s\n", __func__, cmddesc);
+					if (!cpu_online(1)) {
+						ret = cpu_up(1);
 						if (ret)
 							goto blk_out;
+					}
+				} else {
+					dm_dbg("%s: 8, %s\n", __func__, cmddesc);
+					for (i = 1; i < NR_CLUST0_CPUS - 1; i++) {
+						if (!cpu_online(i)) {
+							ret = cpu_up(i);
+							if (ret)
+								goto blk_out;
+						}
 					}
 				}
 			} else if ((cluster1_hotplugged && !do_disable_hotplug) ||
 				(cmd == CMD_CLUST0_IN)) {
-				for (i = 1; i < NR_CLUST0_CPUS; i++) {
-					if (!cpu_online(i)) {
-						ret = cpu_up(i);
+				if (!lcd_is_on) {
+					dm_dbg("%s: 9, %s\n", __func__, cmddesc);
+					if (!cpu_online(1)) {
+						ret = cpu_up(1);
 						if (ret)
 							goto blk_out;
+					}
+				} else {
+					dm_dbg("%s: 10, %s\n", __func__, cmddesc);
+					for (i = 1; i < NR_CLUST0_CPUS; i++) {
+						if (!cpu_online(i)) {
+							ret = cpu_up(i);
+							if (ret)
+								goto blk_out;
+						}
 					}
 				}
 			} else {
 				if (lcd_is_on) {
+					dm_dbg("%s: 11, %s\n", __func__, cmddesc);
 					for (i = NR_CLUST0_CPUS; i < max_num_cpu; i++) {
 						if (do_hotplug_out)
 							goto blk_out;
@@ -722,6 +778,7 @@ static int __ref __cpu_hotplug(bool out_flag, enum hotplug_cmd cmd)
 						}
 					}
 
+					dm_dbg("%s: 12, %s\n", __func__, cmddesc);
 					for (i = 1; i < NR_CLUST0_CPUS; i++) {
 						if (!cpu_online(i)) {
 							ret = cpu_up(i);
@@ -730,10 +787,10 @@ static int __ref __cpu_hotplug(bool out_flag, enum hotplug_cmd cmd)
 						}
 					}
 				} else {
+					dm_dbg("%s: 13, %s\n", __func__, cmddesc);
 					for (i = 1; i < max_num_cpu; i++) {
 						if (do_hotplug_out && i >= NR_CLUST0_CPUS)
 							goto blk_out;
-
 						if (!cpu_online(i)) {
 							ret = cpu_up(i);
 							if (ret)
@@ -749,6 +806,7 @@ static int __ref __cpu_hotplug(bool out_flag, enum hotplug_cmd cmd)
 		if (do_disable_hotplug)
 			goto blk_out;
 
+		dm_dbg("%s: 14, %s\n", __func__, cmddesc);
 		for (i = max_num_cpu - 1; i > 0; i--) {
 			if (cpu_online(i)) {
 				ret = cpu_down(i);
@@ -760,6 +818,7 @@ static int __ref __cpu_hotplug(bool out_flag, enum hotplug_cmd cmd)
 		if (in_suspend_prepared)
 			goto blk_out;
 
+		dm_dbg("%s: 15, %s\n", __func__, cmddesc);
 		for (i = 1; i < max_num_cpu; i++) {
 			if (!cpu_online(i)) {
 				ret = cpu_up(i);
@@ -925,7 +984,7 @@ static DECLARE_WORK(hotplug_in_work, event_hotplug_in_work);
 
 void event_hotplug_in(void)
 {
-	if (hotplug_wq)
+	if (hotplug_wq && lcd_is_on)
 		queue_work(hotplug_wq, &hotplug_in_work);
 }
 #endif
@@ -1090,7 +1149,7 @@ static enum hotplug_cmd diagnose_condition(void)
 #if defined(CONFIG_ARM_EXYNOS_MP_CPUFREQ)
 	ret = CMD_CLUST0_IN;
 
-	if (cur_load_freq >= cluster0_max_freq)
+	if ((cur_load_freq >= cluster0_max_freq) && lcd_is_on)
 		ret = CMD_NORMAL;
 
 	if ((cur_load_freq > normal_min_freq) ||
