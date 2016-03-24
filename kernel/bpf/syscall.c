031d5b5419be9aac65754ff5fdf5915c0b7ad90b
Daniel Borkmann <daniel@iogearbox.net>
2016-03-25 00:30:25 +0100

    bpf: add missing map_flags to bpf_map_show_fdinfo
    Add map_flags attribute to bpf_map_show_fdinfo(), so that tools like
    tc can check for them when loading objects from a pinned entry, e.g.
    if user intent wrt allocation (BPF_F_NO_PREALLOC) is different to the
    pinned object, it can bail out. Follow-up to 6c9059817432 ("bpf:
    pre-allocate hash map elements"), so that tc can still support this
    with v4.6.
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Chatur27 <jasonbright2709@gmail.com>

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -138,11 +138,13 @@ static int bpf_map_show_fdinfo(struct seq_file *m, struct file *filp)
 		   "map_type:\t%u\n"
 		   "key_size:\t%u\n"
 		   "value_size:\t%u\n"
-		   "max_entries:\t%u\n",
+		   "max_entries:\t%u\n"
+		   "map_flags:\t%#x\n",
 		   map->map_type,
 		   map->key_size,
 		   map->value_size,
-		   map->max_entries);
+		   map->max_entries,
+		   map->map_flags);
 
 	return 0;
 }
