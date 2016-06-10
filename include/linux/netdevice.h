d709b315421a2e58f3d0926029b03c52862529f4
Daniel Borkmann <daniel@iogearbox.net>
2016-06-10 21:19:06 +0200

    bpf: enforce recursion limit on redirects
    Respect the stack's xmit_recursion limit for calls into dev_queue_xmit().
    Currently, they are not handeled by the limiter when attached to clsact's
    egress parent, for example, and a buggy program redirecting it to the
    same device again could run into stack overflow eventually. It would be
    good if we could notify an admin to give him a chance to react. We reuse
    xmit_recursion instead of having one private to eBPF, so that the stack's
    current recursion depth will be taken into account as well. Follow-up to
    commit 3896d655f4d4 ("bpf: introduce bpf_clone_redirect() helper") and
    27b29f63058d ("bpf: add bpf_redirect() helper").
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Chatur27 <jasonbright2709@gmail.com>
    Change-Id: I4400066df81ee7a237fa3bf7973bd319ddc4f2fc

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -1840,6 +1840,8 @@ static inline void input_queue_tail_incr_save(struct softnet_data *sd,
 }
 
 DECLARE_PER_CPU_ALIGNED(struct softnet_data, softnet_data);
+#define XMIT_RECURSION_LIMIT   10
+DECLARE_PER_CPU(int, xmit_recursion);
 
 extern void __netif_schedule(struct Qdisc *q);
 
