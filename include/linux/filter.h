94825847f0c395220d1f301abadd27d4ab64696f
Daniel Borkmann <daniel@iogearbox.net>
2016-06-28 12:18:27 +0200

    bpf: add bpf_skb_change_proto helper
    This patch adds a minimal helper for doing the groundwork of changing
    the skb->protocol in a controlled way. Currently supported is v4 to
    v6 and vice versa transitions, which allows f.e. for a minimal, static
    nat64 implementation where applications in containers that still
    require IPv4 can be transparently operated in an IPv6-only environment.
    For example, host facing veth of the container can transparently do
    the transitions in a programmatic way with the help of clsact qdisc
    and cls_bpf.
    
    Idea is to separate concerns for keeping complexity of the helper
    lower, which means that the programs utilize bpf_skb_change_proto(),
    bpf_skb_store_bytes() and bpf_lX_csum_replace() to get the job done,
    instead of doing everything in a single helper (and thus partially
    duplicating helper functionality). Also, bpf_skb_change_proto()
    shouldn't need to deal with raw packet data as this is done by other
    helpers.
    
    bpf_skb_proto_6_to_4() and bpf_skb_proto_4_to_6() unclone the skb to
    operate on a private one, push or pop additionally required header
    space and migrate the gso/gro meta data from the shared info. We do
    mark the gso type as dodgy so that headers are checked and segs
    recalculated by the gso/gro engine. The gso_size target is adapted
    as well. The flags argument added is currently reserved and can be
    used for future extensions.
    
    Change-Id: I8f9c10ba2c548a85be903f07c14d4254df419702
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Chatur27 <jasonbright2709@gmail.com>

--- include/linux/filter.h
+++ include/linux/filter.h
@@ -455,6 +455,7 @@ void sk_filter_uncharge(struct sock *sk, struct sk_filter *fp);
 u64 __bpf_call_base(u64 r1, u64 r2, u64 r3, u64 r4, u64 r5);
 
 struct bpf_prog *bpf_int_jit_compile(struct bpf_prog *prog);
+bool bpf_helper_changes_skb_data(void *func);
 
 struct bpf_prog *bpf_patch_insn_single(struct bpf_prog *prog, u32 off,
 				       const struct bpf_insn *patch, u32 len);
