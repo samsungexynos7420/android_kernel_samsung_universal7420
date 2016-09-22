d9427d90d4c6c5cea9d9f1e5835f65b5c5c0fd21
Daniel Borkmann <daniel@iogearbox.net>
2016-09-23 01:28:37 +0200

    bpf: add helper to invalidate hash
    Add a small helper that complements 36bbef52c7eb ("bpf: direct packet
    write and access for helpers for clsact progs") for invalidating the
    current skb->hash after mangling on headers via direct packet write.
    
    Change-Id: Iaa323f7ec16edfafc721cbad156a7b7cf0794888
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Chatur27 <jasonbright2709@gmail.com>

--- net/core/filter.c
+++ net/core/filter.c
@@ -1685,6 +1685,23 @@ static const struct bpf_func_proto bpf_get_hash_recalc_proto = {
 	.arg1_type	= ARG_PTR_TO_CTX,
 };
 
+BPF_CALL_1(bpf_set_hash_invalid, struct sk_buff *, skb)
+{
+	/* After all direct packet write, this can be used once for
+	 * triggering a lazy recalc on next skb_get_hash() invocation.
+	 */
+	skb->rxhash = 0;
+	skb->l4_rxhash = 0;
+	return 0;
+}
+
+static const struct bpf_func_proto bpf_set_hash_invalid_proto = {
+	.func		= bpf_set_hash_invalid,
+	.gpl_only	= false,
+	.ret_type	= RET_INTEGER,
+	.arg1_type	= ARG_PTR_TO_CTX,
+};
+
 static int bpf_skb_generic_push(struct sk_buff *skb, u32 off, u32 len)
 {
 	/* Caller already did skb_cow() with len as headroom,
@@ -2157,6 +2174,8 @@ tc_cls_act_func_proto(enum bpf_func_id func_id)
 		return &bpf_get_route_realm_proto;
 	case BPF_FUNC_get_hash_recalc:
 		return &bpf_get_hash_recalc_proto;
+	case BPF_FUNC_set_hash_invalid:
+		return &bpf_set_hash_invalid_proto;
 	case BPF_FUNC_perf_event_output:
 		return &bpf_skb_event_output_proto;
 	case BPF_FUNC_get_smp_processor_id:
