c41db8f6ed3abf1bbd0e4c3bd3d96d4d1dd41a1e
Hybridmax <hybridmax95@gmail.com>
2017-09-22 00:41:48 +0200

    sec_battery.c: STORE_MODE: fix store_mode control
    Signed-off-by: morogoku <morogoku@hotmail.com>

--- drivers/battery/sec_battery.c
+++ drivers/battery/sec_battery.c
@@ -4776,22 +4776,14 @@ ssize_t sec_bat_store_attrs(
 		break;
 	case STORE_MODE:
 		if (sscanf(buf, "%d\n", &x) == 1) {
-			if (x) {
-				battery->store_mode = true;
-#if !defined(CONFIG_SEC_FACTORY)
-				if (battery->store_mode) {
-					if (battery->capacity <= 5) {
-						battery->ignore_store_mode = true;
-					} else {
-						union power_supply_propval value;
-						value.intval = battery->store_mode;
-						psy_do_property(battery->pdata->charger_name, set,
-								POWER_SUPPLY_PROP_CONSTANT_CHARGE_CURRENT_MAX, value);
-					}
-				}
-#endif
-			}
+			battery->store_mode = x ? true : false;
 			ret = count;
+			if (battery->store_mode) {
+				union power_supply_propval value;
+				value.intval = battery->store_mode;
+				psy_do_property(battery->pdata->charger_name, set,
+						POWER_SUPPLY_PROP_CONSTANT_CHARGE_CURRENT_MAX, value);
+			}
 		}
 		break;
 	case UPDATE:
@@ -5754,15 +5746,6 @@ static int sec_bat_get_property(struct power_supply *psy,
 			} else
 #endif
 				val->intval = battery->status;
-#if defined(CONFIG_STORE_MODE)
-			psy_do_property("ac", get, POWER_SUPPLY_PROP_ONLINE, value_ac);
-			psy_do_property("usb", get, POWER_SUPPLY_PROP_ONLINE, value_usb);
-			if (battery->store_mode && !lpcharge &&
-				(value_ac.intval || value_usb.intval) &&
-				(battery->status == POWER_SUPPLY_STATUS_DISCHARGING)) {
-				val->intval = POWER_SUPPLY_STATUS_CHARGING;
-			}
-#endif
 		}
 		break;
 	case POWER_SUPPLY_PROP_CHARGE_TYPE:
@@ -7703,8 +7686,14 @@ static int __devinit sec_battery_probe(struct platform_device *pdev)
 	battery->cable_type = POWER_SUPPLY_TYPE_BATTERY;
 	battery->test_mode = 0;
 	battery->factory_mode = false;
+#if defined(CONFIG_STORE_MODE)
 	battery->store_mode = false;
-	battery->ignore_store_mode = false;
+	value.intval = battery->store_mode;
+	psy_do_property(battery->pdata->charger_name, set,
+			POWER_SUPPLY_PROP_CONSTANT_CHARGE_CURRENT_MAX, value);
+#else
+	battery->store_mode = STORE_MODE_NONE;
+#endif
 	battery->slate_mode = false;
 	slate_mode_state = battery->slate_mode;
 	battery->is_hc_usb = false;
