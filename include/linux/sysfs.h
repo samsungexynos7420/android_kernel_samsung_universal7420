2b518f88820702cf919553f8221c7d3846e63156
Bart Van Assche <bart.vanassche@wdc.com>
2018-08-02 10:51:40 -0700

    scsi: sysfs: Introduce sysfs_{un,}break_active_protection()
    commit 2afc9166f79b8f6da5f347f48515215ceee4ae37 upstream.
    
    Introduce these two functions and export them such that the next patch
    can add calls to these functions from the SCSI core.
    
    Signed-off-by: Bart Van Assche <bart.vanassche@wdc.com>
    Acked-by: Tejun Heo <tj@kernel.org>
    Acked-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- include/linux/sysfs.h
+++ include/linux/sysfs.h
@@ -196,6 +196,9 @@ int __must_check sysfs_chmod_file(struct kobject *kobj,
 				  const struct attribute *attr, umode_t mode);
 int __must_check sysfs_chown_file(struct kobject *kobj,
 				  const struct attribute *attr, uid_t uid, gid_t gid);
+struct kernfs_node *sysfs_break_active_protection(struct kobject *kobj,
+						  const struct attribute *attr);
+void sysfs_unbreak_active_protection(struct kernfs_node *kn);
 void sysfs_remove_file_ns(struct kobject *kobj, const struct attribute *attr,
 			  const void *ns);
 bool sysfs_remove_file_self(struct kobject *kobj, const struct attribute *attr);
@@ -293,6 +296,17 @@ static inline int sysfs_chmod_file(struct kobject *kobj,
 				   const struct attribute *attr, umode_t mode)
 {
 	return 0;
+}
+
+static inline struct kernfs_node *
+sysfs_break_active_protection(struct kobject *kobj,
+			      const struct attribute *attr)
+{
+	return NULL;
+}
+
+static inline void sysfs_unbreak_active_protection(struct kernfs_node *kn)
+{
 }
 
 static inline void sysfs_remove_file_ns(struct kobject *kobj,
