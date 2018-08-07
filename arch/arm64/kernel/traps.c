b53f3f8b99492c5f1abd12a287cf22dc2b0d8793
Hari Vyas <hari.vyas@broadcom.com>
2018-08-07 16:33:48 +0530

    arm64: fix for bad_mode() handler to always result in panic
    [ Upstream commit e4ba15debcfd27f60d43da940a58108783bff2a6 ]
    
    The bad_mode() handler is called if we encounter an uunknown exception,
    with the expectation that the subsequent call to panic() will halt the
    system. Unfortunately, if the exception calling bad_mode() is taken from
    EL0, then the call to die() can end up killing the current user task and
    calling schedule() instead of falling through to panic().
    
    Remove the die() call altogether, since we really want to bring down the
    machine in this "impossible" case.
    
    Signed-off-by: Hari Vyas <hari.vyas@broadcom.com>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Change-Id: Id6fae0e1f677e35efa39981c00c5ac84fef7889f

--- arch/arm64/kernel/traps.c
+++ arch/arm64/kernel/traps.c
@@ -486,7 +486,6 @@ asmlinkage void bad_mode(struct pt_regs *regs, int reason, unsigned int esr)
 	pr_crit("Bad mode in %s handler detected, code 0x%08x\n",
 		handler[reason], esr);
 
-	die("Oops - bad mode", regs, 0);
 	local_irq_disable();
 	panic("bad mode");
 }
