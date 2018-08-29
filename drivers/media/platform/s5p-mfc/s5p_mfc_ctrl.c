6977fb26c5909c5b7abbc2e272ddc9683dc79b73
Jeonghee Kim <jhhhh.kim@samsung.com>
2018-08-29 16:03:45 +0900

    [COMMON] media: mfc: call release_firmware function
    To prevent resource leck problem,
    call release_firmware function if request_firmware returns error.
    
    Change-Id: I871248a6901355984dcf84b69720cadbe6f58a95
    Signed-off-by: Jeonghee Kim <jhhhh.kim@samsung.com>

--- drivers/media/platform/s5p-mfc/s5p_mfc_ctrl.c
+++ drivers/media/platform/s5p-mfc/s5p_mfc_ctrl.c
@@ -90,6 +90,7 @@ int s5p_mfc_load_firmware(struct s5p_mfc_dev *dev)
 				     dev->variant->fw_name, dev->v4l2_dev.dev);
 	if (err != 0) {
 		mfc_err("Firmware is not present in the /lib/firmware directory nor compiled in kernel\n");
+		release_firmware(fw_blob);
 		return -EINVAL;
 	}
 	if (fw_blob->size > dev->fw_size) {
