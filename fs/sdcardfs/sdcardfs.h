e4a27bb1ea975b1bdef1f56a27d1c431836f1440
Daniel Rosenberg <drosen@google.com>
2018-10-25 16:25:15 -0700

    ANDROID: sdcardfs: Add option to not link obb
    Add mount option unshared_obb to not link the obb
    folders of multiple users together.
    
    Bug: 27915347
    Test: mount with option. Check if altering one obb
          alters the other
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    
    Change-Id: I3956e06bd0a222b0bbb2768c9a8a8372ada85e1e
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>
    Signed-off-by: Lunarixus <Lunarixus@raphielgang.org>
    
    Update sdcardfs.h

--- fs/sdcardfs/sdcardfs.h
+++ fs/sdcardfs/sdcardfs.h
@@ -220,6 +220,8 @@ struct sdcardfs_mount_options {
 	userid_t fs_user_id;
 	bool multiuser;
 	bool gid_derivation;
+	bool default_normal;
+	bool unshared_obb;
 	unsigned int reserved_mb;
 	bool nocache;
 };
