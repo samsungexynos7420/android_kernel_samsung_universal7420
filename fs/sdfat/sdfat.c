4bdac2ffae0a511d405d87a5ad3ccd7bd3f889ee
K9100ii <k9100ii.ki@gmail.com>
2021-01-28 15:14:52 +0000

    sdfat: remove the second 'to' argument in a call to truncate_pagecache()
    truncate_pagecache()s second parameter was removed in commit "truncate:
    drop 'oldsize' truncate_pagecache() parameter".
    
    Signed-off-by: K9100ii <k9100ii.ki@gmail.com>

--- fs/sdfat/sdfat.c
+++ fs/sdfat/sdfat.c
@@ -456,7 +456,7 @@ static inline void __sdfat_set_bio_iterate(struct bio *bio, sector_t sector,
 static void __sdfat_truncate_pagecache(struct inode *inode,
 					loff_t to, loff_t newsize)
 {
-	truncate_pagecache(inode, to, newsize);
+	truncate_pagecache(inode, newsize);
 }
 
 static int sdfat_d_hash(const struct dentry *dentry,
