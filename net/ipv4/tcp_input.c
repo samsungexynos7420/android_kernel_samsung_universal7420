6916e5384cd1acaea23f6cd5111df47afc746f33
TALU <taluemail272@gmail.com>
2021-02-05 23:59:08 +0000

    Revert "tcp: fix xmit timer to only be reset if data ACKed/SACKed"
    This reverts commit 825841d4f72f08dff15a15bb99f45efac229b964.

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -118,7 +118,6 @@ int sysctl_tcp_default_init_rwnd __read_mostly = TCP_INIT_CWND * 2;
 #define FLAG_ORIG_SACK_ACKED	0x200 /* Never retransmitted data are (s)acked	*/
 #define FLAG_SND_UNA_ADVANCED	0x400 /* Snd_una was changed (!= FLAG_DATA_ACKED) */
 #define FLAG_DSACKING_ACK	0x800 /* SACK blocks contained D-SACK info */
-#define FLAG_SET_XMIT_TIMER	0x1000 /* Set TLP or RTO timer */
 #define FLAG_SACK_RENEGING	0x2000 /* snd_una advanced to a sacked seq */
 #define FLAG_UPDATE_TS_RECENT	0x4000 /* tcp_replace_ts_recent() */
 #define FLAG_NO_CHALLENGE_ACK	0x8000 /* do not call tcp_send_challenge_ack()	*/
@@ -3037,13 +3036,6 @@ void tcp_resume_early_retransmit(struct sock *sk)
 	tcp_xmit_retransmit_queue(sk);
 }
 
-/* Try to schedule a loss probe; if that doesn't work, then schedule an RTO. */
-static void tcp_set_xmit_timer(struct sock *sk)
-{
-	if (!tcp_schedule_loss_probe(sk))
-		tcp_rearm_rto(sk);
-}
-
 /* If we get here, the whole TSO packet has not been acked. */
 #ifndef CONFIG_MPTCP
 static
@@ -3178,7 +3170,7 @@ static int tcp_clean_rtx_queue(struct sock *sk, int prior_fackets,
 		}
 
 		tcp_ack_update_rtt(sk, flag, seq_rtt);
-		flag |= FLAG_SET_XMIT_TIMER;  /* set TLP or RTO timer */
+		tcp_rearm_rto(sk);
 
 		if (tcp_is_reno(tp)) {
 			tcp_remove_reno_sacks(sk, pkts_acked);
@@ -3474,6 +3466,10 @@ static int tcp_ack(struct sock *sk,
 	if (after(ack, tp->snd_nxt))
 		goto invalid_ack;
 
+	if (icsk->icsk_pending == ICSK_TIME_EARLY_RETRANS ||
+	    icsk->icsk_pending == ICSK_TIME_LOSS_PROBE)
+		tcp_rearm_rto(sk);
+
 	if (after(ack, prior_snd_una))
 		flag |= FLAG_SND_UNA_ADVANCED;
 
@@ -3530,12 +3526,6 @@ static int tcp_ack(struct sock *sk,
 	flag |= tcp_clean_rtx_queue(sk, prior_fackets, prior_snd_una);
 	acked -= tp->packets_out;
 
-	if (tp->tlp_high_seq)
-		tcp_process_tlp_ack(sk, ack, flag);
-	/* If needed, reset TLP/RTO timer; RACK may later override this. */
-	if (flag & FLAG_SET_XMIT_TIMER)
-		tcp_set_xmit_timer(sk);
-
 #ifdef CONFIG_MPTCP
 	if (mptcp(tp)) {
 		if (mptcp_fallback_infinite(sk, flag)) {
@@ -3560,6 +3550,9 @@ static int tcp_ack(struct sock *sk,
 			tcp_cong_avoid(sk, ack, prior_in_flight);
 	}
 
+	if (tp->tlp_high_seq)
+		tcp_process_tlp_ack(sk, ack, flag);
+
 	if ((flag & FLAG_FORWARD_PROGRESS) || !(flag & FLAG_NOT_DUP)) {
 		struct dst_entry *dst = __sk_dst_get(sk);
 		if (dst)
@@ -3567,6 +3560,8 @@ static int tcp_ack(struct sock *sk,
 	}
 
 #ifndef CONFIG_MPTCP
+	if (icsk->icsk_pending == ICSK_TIME_RETRANS)
+		tcp_schedule_loss_probe(sk);
 	if (tp->srtt != prior_rtt || tp->snd_cwnd != prior_cwnd)
 		tcp_update_pacing_rate(sk);
 #endif
