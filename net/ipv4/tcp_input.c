04397479b9565e0a797d07f01db2cd6f940725a2
K9100ii <k9100ii.ki@gmail.com>
2021-02-06 00:09:13 +0000

    Revert "net: tcp: Scale the TCP backlog queue to absorb packet bursts"
    This reverts commit 27adc682bf05227c57f2ce781e075fa701bfe24f.
    
    Signed-off-by: K9100ii <k9100ii.ki@gmail.com>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -4284,8 +4284,7 @@ static int tcp_try_rmem_schedule(struct sock *sk, struct sk_buff *skb,
 		sk = mptcp_meta_sk(sk);
 #endif
 
-	if (atomic_read(&sk->sk_rmem_alloc)
-				     > ((sk->sk_rcvbuf + sk->sk_sndbuf) * 4) ||
+	if (atomic_read(&sk->sk_rmem_alloc) > sk->sk_rcvbuf ||
 	    !sk_rmem_schedule(sk, skb, size)) {
 
 		if (tcp_prune_queue(sk) < 0)
