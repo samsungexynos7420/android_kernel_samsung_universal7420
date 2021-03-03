b8ff9b0ef6128cbeafe24e81ec5ad7b1829b8273
TALU <taluemail272@gmail.com>
2021-03-03 20:51:18 +0000

    sdfat: remove (what are now) redefinitions of inode_lock() and inode_unlock()
    Signed-off-by: TALU <taluemail272@gmail.com>

--- fs/sdfat/sdfat.c
+++ fs/sdfat/sdfat.c
@@ -199,20 +199,6 @@ static inline unsigned long __sdfat_init_name_hash(const struct dentry *unused)
 }
 #endif
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 4, 21)
-	/* EMPTY */
-#else /* LINUX_VERSION_CODE < KERNEL_VERSION(4, 4, 21) */
-static inline void inode_lock(struct inode *inode)
-{
-	mutex_lock(&inode->i_mutex);
-}
-
-static inline void inode_unlock(struct inode *inode)
-{
-	mutex_unlock(&inode->i_mutex);
-}
-#endif
-
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 3, 0)
 static void sdfat_writepage_end_io(struct bio *bio)
 {
