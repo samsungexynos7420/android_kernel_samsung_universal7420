663bef52225fd711617d4499616d5f0ff3b14a89
7FM <7fm.gerrit@gmail.com>
2021-04-30 16:36:09 +0200

    scripts: add HOSTCFLAGS compile arguments for utility compilation
    Change-Id: Iea6252706ac86b31e0a8fdfd0d80ce99a2870f3f

--- scripts/fips_fmp_hmac.sh
+++ scripts/fips_fmp_hmac.sh
@@ -120,7 +120,7 @@ if [[ ! -f offsets_sizes.txt ]]; then
 fi
 
 rm -f fips_fmp_utils
-$HOSTCC -o fips_fmp_utils $srctree/scripts/fips_fmp_utils.c
+$HOSTCC $HOSTCFLAGS -o fips_fmp_utils $srctree/scripts/fips_fmp_utils.c
 retval=$?
 if [ $retval -ne 0 ]; then
 	echo "$0 : $HOSTCC returned error"
