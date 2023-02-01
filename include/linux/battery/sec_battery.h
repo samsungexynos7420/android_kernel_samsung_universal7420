6e88c83186fdcfc2e783322b9cca387d7ae80189
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2023-02-01 15:54:45 +0000

    battery: sec_battery: export {CURRENT/VOLTAGE}_MAX to sysfs
    Change-Id: I54c775bb80c2151bdc69ea9fb53a48a34327bbef

--- include/linux/battery/sec_battery.h
+++ include/linux/battery/sec_battery.h
@@ -104,6 +104,7 @@ struct sec_battery_info {
 	int current_isys_now;	/* isys current (mA) */
 	int current_isys_avg;	/* isys avg current (mA) */
 
+	unsigned int input_voltage;
 	unsigned int capacity;			/* SOC (%) */
 
 
