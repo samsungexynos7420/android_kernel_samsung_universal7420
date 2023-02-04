2f9330f7aeb6a000b16bdea60c4680b20815c81c
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2023-02-04 21:30:02 +0000

    wtf is this

--- drivers/battery/sec_battery.c
+++ drivers/battery/sec_battery.c
@@ -7689,7 +7689,7 @@ static int __devinit sec_battery_probe(struct platform_device *pdev)
 	psy_do_property(battery->pdata->charger_name, set,
 			POWER_SUPPLY_PROP_CONSTANT_CHARGE_CURRENT_MAX, value);
 #else
-	battery->store_mode = STORE_MODE_NONE;
+	battery->store_mode = false;
 #endif
 	battery->slate_mode = false;
 	slate_mode_state = battery->slate_mode;
