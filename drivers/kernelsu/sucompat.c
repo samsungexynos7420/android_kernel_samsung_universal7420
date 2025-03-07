de58082bf718018c0ccd78bed9ca23b04ee0392a
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-03-07 13:02:50 +0800

    KernelSU: adapt to CONFIG_UIDGID_STRICT_TYPE_CHECKS=n
    this kernel has to be built with CONFIG_UIDGID_STRICT_TYPE_CHECKS=n
    .val has to be removed as uid not a struct
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- drivers/kernelsu/sucompat.c
+++ drivers/kernelsu/sucompat.c
@@ -58,7 +58,7 @@ int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
 	if (unlikely(!ksu_sucompat_non_kp))
 		return 0;
 	
-	if (!ksu_is_allow_uid(current_uid().val)) {
+	if (!ksu_is_allow_uid(current_uid())) {
 		return 0;
 	}
 
@@ -85,7 +85,7 @@ int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags)
 	if (unlikely(!ksu_sucompat_non_kp))
 		return 0;
 
-	if (!ksu_is_allow_uid(current_uid().val)) {
+	if (!ksu_is_allow_uid(current_uid())) {
 		return 0;
 	}
 
@@ -130,7 +130,7 @@ int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
 	if (unlikely(!ksu_sucompat_non_kp))
 		return 0;
 	
-	if (!ksu_is_allow_uid(current_uid().val))
+	if (!ksu_is_allow_uid(current_uid()))
 		return 0;
 
 	if (unlikely(!filename_ptr))
@@ -163,7 +163,7 @@ int ksu_handle_execve_sucompat(int *fd, const char __user **filename_user,
 	if (unlikely(!ksu_sucompat_non_kp))
 		return 0;
 
-	if (!ksu_is_allow_uid(current_uid().val))
+	if (!ksu_is_allow_uid(current_uid()))
 		return 0;
 	
 	if (unlikely(!filename_user))
@@ -205,7 +205,7 @@ int ksu_handle_devpts(struct inode *inode)
 		return 0;
 	}
 
-	uid_t uid = current_uid().val;
+	uid_t uid = current_uid();
 	if (uid % 100000 < 10000) {
 		// not untrusted_app, ignore it
 		return 0;
