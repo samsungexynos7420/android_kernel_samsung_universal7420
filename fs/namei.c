fb6bc34b7769c85c1834aa775128c405f57a16f4
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-03-10 21:21:43 +0800

    fs: adapt to ovl 4.1

--- fs/namei.c
+++ fs/namei.c
@@ -2465,23 +2465,6 @@ kern_path_mountpoint(int dfd, const char *name, struct path *path,
 EXPORT_SYMBOL(kern_path_mountpoint);
 
 /*
- * It's inline, so penalty for filesystems that don't use sticky bit is
- * minimal.
- */
-static inline int check_sticky(struct inode *dir, struct inode *inode)
-{
-	kuid_t fsuid = current_fsuid();
-
-	if (!(dir->i_mode & S_ISVTX))
-		return 0;
-	if (uid_eq(inode->i_uid, fsuid))
-		return 0;
-	if (uid_eq(dir->i_uid, fsuid))
-		return 0;
-	return !capable_wrt_inode_uidgid(inode, CAP_FOWNER);
-}
-
-/*
  *	Check whether we can remove a link victim from directory dir, check
  *  whether the type of victim is right.
  *  1. We can't do it if dir is read-only (done in permission())
