10af7a6a2552f2a9ca85cac46f2474eecc752720
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2025-04-01 14:36:27 +0100

    Allow to toggle off fake cmdline easily

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -2,13 +2,20 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#if defined (CONFIG_PROC_FAKE_CMDLINE)
 #include <asm/setup.h>
 
 static char updated_command_line[COMMAND_LINE_SIZE];
+#endif //CONFIG_PROC_FAKE_CMDLINE
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
+#if defined (CONFIG_PROC_FAKE_CMDLINE)
 	seq_printf(m, "%s\n", updated_command_line);
+#else
+	seq_printf(m, "%s\n", saved_command_line);
+#endif //CONFIG_PROC_FAKE_CMDLINE
+
 	return 0;
 }
 
@@ -24,6 +31,7 @@ static const struct file_operations cmdline_proc_fops = {
 	.release	= single_release,
 };
 
+#if defined (CONFIG_PROC_FAKE_CMDLINE)
 static void proc_cmdline_set(char *name, char *value)
 {
 	char flag_str[COMMAND_LINE_SIZE];
@@ -38,14 +46,16 @@ static void proc_cmdline_set(char *name, char *value)
  	// flag was not found, insert it
 	scnprintf(updated_command_line, COMMAND_LINE_SIZE, "%s %s=%s", updated_command_line, name, value);
 }
-
+#endif //CONFIG_PROC_FAKE_CMDLINE
 static int __init proc_cmdline_init(void)
 {
-		// copy it only once
+#if defined (CONFIG_PROC_FAKE_CMDLINE)
+	// copy it only once
 	strcpy(updated_command_line, saved_command_line);
  	proc_cmdline_set("androidboot.verifiedbootstate", "green");
 	proc_cmdline_set("androidboot.warranty_bit", "0");
 	proc_cmdline_set("androidboot.fmp_config", "1");
+#endif //CONFIG_PROC_FAKE_CMDLINE
 
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
