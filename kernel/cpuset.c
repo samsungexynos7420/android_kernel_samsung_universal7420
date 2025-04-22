72802420fa8380ea099ea50947cf6226e1401408
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2025-04-22 17:10:29 +0100

    Revert "kernel: cpuset: Make cpusets restore on hotplug"
    This reverts commit 9fa298b911a7937db47923695550b1746e706be8.
    
    Upcoming cpuset commits conflict with this. Eventually restore hotplug patch later

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -82,7 +82,6 @@ struct cpuset {
 
 	unsigned long flags;		/* "unsigned long" so bitops work */
 	cpumask_var_t cpus_allowed;	/* CPUs allowed to tasks in cpuset */
-	cpumask_var_t cpus_requested;   /* CPUS requested, but not used because of hotplug */
 	nodemask_t mems_allowed;	/* Memory Nodes allowed to tasks */
 
 	/*
@@ -364,7 +363,7 @@ static void cpuset_update_task_spread_flag(struct cpuset *cs,
 
 static int is_cpuset_subset(const struct cpuset *p, const struct cpuset *q)
 {
-	return	cpumask_subset(p->cpus_requested, q->cpus_requested) &&
+	return	cpumask_subset(p->cpus_allowed, q->cpus_allowed) &&
 		nodes_subset(p->mems_allowed, q->mems_allowed) &&
 		is_cpu_exclusive(p) <= is_cpu_exclusive(q) &&
 		is_mem_exclusive(p) <= is_mem_exclusive(q);
@@ -455,7 +454,7 @@ static int validate_change(struct cpuset *cur, struct cpuset *trial)
 	cpuset_for_each_child(c, css, par) {
 		if ((is_cpu_exclusive(trial) || is_cpu_exclusive(c)) &&
 		    c != cur &&
-		    cpumask_intersects(trial->cpus_requested, c->cpus_requested))
+		    cpumask_intersects(trial->cpus_allowed, c->cpus_allowed))
 			goto out;
 		if ((is_mem_exclusive(trial) || is_mem_exclusive(c)) &&
 		    c != cur &&
@@ -910,18 +909,16 @@ static int update_cpumask(struct cpuset *cs, struct cpuset *trialcs,
 	if (!*buf) {
 		cpumask_clear(trialcs->cpus_allowed);
 	} else {
-		retval = cpulist_parse(buf, trialcs->cpus_requested);
+		retval = cpulist_parse(buf, trialcs->cpus_allowed);
 		if (retval < 0)
 			return retval;
 
-		if (!cpumask_subset(trialcs->cpus_requested, cpu_present_mask))
+		if (!cpumask_subset(trialcs->cpus_allowed, cpu_active_mask))
 			return -EINVAL;
-
-		cpumask_and(trialcs->cpus_allowed, trialcs->cpus_requested, cpu_active_mask);
 	}
 
 	/* Nothing to do if the cpus didn't change */
-	if (cpumask_equal(cs->cpus_requested, trialcs->cpus_requested))
+	if (cpumask_equal(cs->cpus_allowed, trialcs->cpus_allowed))
 		return 0;
 
 	retval = validate_change(cs, trialcs);
@@ -932,7 +929,6 @@ static int update_cpumask(struct cpuset *cs, struct cpuset *trialcs,
 
 	mutex_lock(&callback_mutex);
 	cpumask_copy(cs->cpus_allowed, trialcs->cpus_allowed);
-	cpumask_copy(cs->cpus_requested, trialcs->cpus_requested);
 	mutex_unlock(&callback_mutex);
 
 	update_tasks_cpumask_hier(cs, true);
@@ -1695,7 +1691,7 @@ static int cpuset_common_seq_show(struct seq_file *sf, void *v)
 
 	switch (type) {
 	case FILE_CPULIST:
-		s += cpulist_scnprintf(s, count, cs->cpus_requested);
+		s += cpulist_scnprintf(s, count, cs->cpus_allowed);
 		break;
 	case FILE_MEMLIST:
 		s += nodelist_scnprintf(s, count, cs->mems_allowed);
@@ -1875,25 +1871,18 @@ cpuset_css_alloc(struct cgroup_subsys_state *parent_css)
 	cs = kzalloc(sizeof(*cs), GFP_KERNEL);
 	if (!cs)
 		return ERR_PTR(-ENOMEM);
-	if (!alloc_cpumask_var(&cs->cpus_allowed, GFP_KERNEL))
-		goto error_allowed;
-	if (!alloc_cpumask_var(&cs->cpus_requested, GFP_KERNEL))
-		goto error_requested;
+	if (!alloc_cpumask_var(&cs->cpus_allowed, GFP_KERNEL)) {
+		kfree(cs);
+		return ERR_PTR(-ENOMEM);
+	}
 
 	set_bit(CS_SCHED_LOAD_BALANCE, &cs->flags);
 	cpumask_clear(cs->cpus_allowed);
-	cpumask_clear(cs->cpus_requested);
 	nodes_clear(cs->mems_allowed);
 	fmeter_init(&cs->fmeter);
 	cs->relax_domain_level = -1;
 
 	return &cs->css;
-
-error_requested:
-	free_cpumask_var(cs->cpus_allowed);
-error_allowed:
-	kfree(cs);
-	return ERR_PTR(-ENOMEM);
 }
 
 static int cpuset_css_online(struct cgroup_subsys_state *css)
@@ -1944,7 +1933,6 @@ static int cpuset_css_online(struct cgroup_subsys_state *css)
 	mutex_lock(&callback_mutex);
 	cs->mems_allowed = parent->mems_allowed;
 	cpumask_copy(cs->cpus_allowed, parent->cpus_allowed);
-	cpumask_copy(cs->cpus_requested, parent->cpus_requested);
 	mutex_unlock(&callback_mutex);
 out_unlock:
 	mutex_unlock(&cpuset_mutex);
@@ -1977,7 +1965,6 @@ static void cpuset_css_free(struct cgroup_subsys_state *css)
 	struct cpuset *cs = css_cs(css);
 
 	free_cpumask_var(cs->cpus_allowed);
-	free_cpumask_var(cs->cpus_requested);
 	kfree(cs);
 }
 
@@ -2005,11 +1992,8 @@ int __init cpuset_init(void)
 
 	if (!alloc_cpumask_var(&top_cpuset.cpus_allowed, GFP_KERNEL))
 		BUG();
-	if (!alloc_cpumask_var(&top_cpuset.cpus_requested, GFP_KERNEL))
-		BUG();
 
 	cpumask_setall(top_cpuset.cpus_allowed);
-	cpumask_setall(top_cpuset.cpus_requested);
 	nodes_setall(top_cpuset.mems_allowed);
 
 	fmeter_init(&top_cpuset.fmeter);
@@ -2064,7 +2048,7 @@ static void remove_tasks_in_empty_cpuset(struct cpuset *cs)
  */
 static void cpuset_hotplug_update_tasks(struct cpuset *cs)
 {
-	static cpumask_t diff, new_allowed;
+	static cpumask_t off_cpus;
 	static nodemask_t off_mems;
 	bool is_empty;
 	bool on_dfl = cgroup_on_dfl(cs->css.cgroup);
@@ -2083,12 +2067,11 @@ retry:
 		goto retry;
 	}
 
-	cpumask_and(&new_allowed, cs->cpus_requested, top_cpuset.cpus_allowed);
-	cpumask_xor(&diff, &new_allowed, cs->cpus_allowed);
+	cpumask_andnot(&off_cpus, cs->cpus_allowed, top_cpuset.cpus_allowed);
 	nodes_andnot(off_mems, cs->mems_allowed, top_cpuset.mems_allowed);
 
 	mutex_lock(&callback_mutex);
-	cpumask_copy(cs->cpus_allowed, &new_allowed);
+	cpumask_andnot(cs->cpus_allowed, cs->cpus_allowed, &off_cpus);
 	mutex_unlock(&callback_mutex);
 
 	/*
@@ -2098,7 +2081,7 @@ retry:
 	 * in it will be migrated to an ancestor.
 	 */
 	if ((on_dfl && cpumask_empty(cs->cpus_allowed)) ||
-	    (!cpumask_empty(&diff) && !cpumask_empty(cs->cpus_allowed)))
+	    (!cpumask_empty(&off_cpus) && !cpumask_empty(cs->cpus_allowed)))
 		update_tasks_cpumask(cs);
 
 	mutex_lock(&callback_mutex);
