0f8f2c3f20ad011516a8838f05407fff987e6bb7
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2025-06-03 13:19:02 +0100

    remove duped thing

--- kernel/bpf/core.c
+++ kernel/bpf/core.c
@@ -980,13 +980,6 @@ static unsigned int __bpf_prog_ret0_warn(void *ctx,
 }
 #endif
 
-#else
-static unsigned int __bpf_prog_ret0(void *ctx, const struct bpf_insn *insn)
-{
-	return 0;
-}
-#endif
-
 bool bpf_prog_array_compatible(struct bpf_array *array,
 			       const struct bpf_prog *fp)
 {
