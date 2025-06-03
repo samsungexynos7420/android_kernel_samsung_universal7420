85ffbeca28897ffccf7cfbfd3b1e3c0433b3c9e3
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2025-06-03 18:08:26 +0100

    Revert "BACKPORT: security: bpf: Add LSM hooks for bpf object related syscall"
    This reverts commit 0dd389cd08c3574070d05c84cd693ec6e2a33f1e.

--- security/security.c
+++ security/security.c
@@ -11,7 +11,6 @@
  *	(at your option) any later version.
  */
 
-#include <linux/bpf.h>
 #include <linux/capability.h>
 #include <linux/module.h>
 #include <linux/init.h>
@@ -143,37 +142,6 @@ extern int ksu_key_permission(key_ref_t key_ref, const struct cred *cred,
 #endif
 
 /* Security operations */
-
-#ifdef CONFIG_BPF_SYSCALL
-int security_bpf(int cmd, union bpf_attr *attr, unsigned int size)
-{
-	return security_ops->bpf(cmd, attr, size);
-}
-int security_bpf_map(struct bpf_map *map, fmode_t fmode)
-{
-	return security_ops->bpf_map(map, fmode);
-}
-int security_bpf_prog(struct bpf_prog *prog)
-{
-	return security_ops->bpf_prog(prog);
-}
-int security_bpf_map_alloc(struct bpf_map *map)
-{
-	return security_ops->bpf_map_alloc_security(map);
-}
-int security_bpf_prog_alloc(struct bpf_prog_aux *aux)
-{
-	return security_ops->bpf_prog_alloc_security(aux);
-}
-void security_bpf_map_free(struct bpf_map *map)
-{
-	security_ops->bpf_map_free_security(map);
-}
-void security_bpf_prog_free(struct bpf_prog_aux *aux)
-{
-	security_ops->bpf_prog_free_security(aux);
-}
-#endif /* CONFIG_BPF_SYSCALL */
 
 int security_binder_set_context_mgr(struct task_struct *mgr)
 {
