4ff7a307ff256f6c8dfc1e45930daf3c2c8309c6
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2025-06-11 10:34:10 +0100

    backport MEMCG_FORCE_USE_VM_SWAPPINESS from 8890
    and enable it

--- include/linux/swap.h
+++ include/linux/swap.h
@@ -349,8 +349,15 @@ static inline void scan_unevictable_unregister_node(struct node *node)
 
 extern int kswapd_run(int nid);
 extern void kswapd_stop(int nid);
-#ifdef CONFIG_MEMCG
-extern int mem_cgroup_swappiness(struct mem_cgroup *mem);
+#if defined(CONFIG_MEMCG) && !(CONFIG_MEMCG_FORCE_USE_VM_SWAPPINESS)
+static inline int mem_cgroup_swappiness(struct mem_cgroup *memcg)
+{
+	/* root ? */
+	if (mem_cgroup_disabled() || !memcg->css.parent)
+		return vm_swappiness;
+
+	return memcg->swappiness;
+}
 #else
 static inline int mem_cgroup_swappiness(struct mem_cgroup *mem)
 {
