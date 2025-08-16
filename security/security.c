3d1bbbf649591331ab7b46235dc71c8d4b168f97
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2025-08-16 09:57:01 +0100

    Revert "KernelSU: manual security hooks"
    This reverts commit 6c9ffd1a81a1f6962c2d99b693684fe57dcb9a30.

--- security/security.c
+++ security/security.c
@@ -133,18 +133,6 @@ int __init register_security(struct security_operations *ops)
 	return 0;
 }
 
-#ifdef CONFIG_KSU
-extern int ksu_handle_prctl(int option, unsigned long arg2, unsigned long arg3,
-		     unsigned long arg4, unsigned long arg5);
-extern int ksu_handle_rename(struct dentry *old_dentry, struct dentry *new_dentry);
-extern int ksu_handle_setuid(struct cred *new, const struct cred *old);
-extern int ksu_key_permission(key_ref_t key_ref, const struct cred *cred,
-			      unsigned perm);
-extern int ksu_sb_mount(const char *dev_name, const struct path *path,
-                        const char *type, unsigned long flags, void *data);
-extern int ksu_inode_permission(struct inode *inode, int mask);
-#endif
-
 /* Security operations */
 
 #ifdef CONFIG_BPF_SYSCALL
@@ -346,7 +334,6 @@ int security_sb_statfs(struct dentry *dentry)
 int security_sb_mount(const char *dev_name, struct path *path,
                        const char *type, unsigned long flags, void *data)
 {
-	ksu_sb_mount(dev_name, path, type, flags, data);
 	return security_ops->sb_mount(dev_name, path, type, flags, data);
 }
 
@@ -581,9 +568,6 @@ int security_inode_mknod(struct inode *dir, struct dentry *dentry, umode_t mode,
 int security_inode_rename(struct inode *old_dir, struct dentry *old_dentry,
 			   struct inode *new_dir, struct dentry *new_dentry)
 {
-#ifdef CONFIG_KSU
-	ksu_handle_rename(old_dentry, new_dentry);
-#endif
         if (unlikely(IS_PRIVATE(old_dentry->d_inode) ||
             (new_dentry->d_inode && IS_PRIVATE(new_dentry->d_inode))))
 		return 0;
@@ -607,9 +591,6 @@ int security_inode_follow_link(struct dentry *dentry, struct nameidata *nd)
 
 int security_inode_permission(struct inode *inode, int mask)
 {
-#ifdef CONFIG_KSU
-	ksu_inode_permission(inode, mask);
-#endif
 	if (unlikely(IS_PRIVATE(inode)))
 		return 0;
 	return security_ops->inode_permission(inode, mask);
@@ -905,9 +886,6 @@ int security_kernel_module_from_file(struct file *file)
 int security_task_fix_setuid(struct cred *new, const struct cred *old,
 			     int flags)
 {
-#ifdef CONFIG_KSU
-	ksu_handle_setuid(new, old);
-#endif
 	return security_ops->task_fix_setuid(new, old, flags);
 }
 
@@ -982,9 +960,6 @@ int security_task_wait(struct task_struct *p)
 int security_task_prctl(int option, unsigned long arg2, unsigned long arg3,
 			 unsigned long arg4, unsigned long arg5)
 {
-#ifdef CONFIG_KSU
-	ksu_handle_prctl(option, arg2, arg3, arg4, arg5);
-#endif
 #ifdef CONFIG_SECURITY_YAMA_STACKED
 	int rc;
 	rc = yama_task_prctl(option, arg2, arg3, arg4, arg5);
@@ -1469,9 +1444,6 @@ void security_key_free(struct key *key)
 int security_key_permission(key_ref_t key_ref,
 			    const struct cred *cred, key_perm_t perm)
 {
-#ifdef CONFIG_KSU
-	ksu_key_permission(key_ref, cred, perm);
-#endif
 	return security_ops->key_permission(key_ref, cred, perm);
 }
 
