2e421b97da7ffd831d659ddb6a27ff7fda939a8c
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2025-09-06 12:16:59 +0100

    Update namei.c

--- fs/namei.c
+++ fs/namei.c
@@ -4318,10 +4318,10 @@ out:
 EXPORT_SYMBOL(vfs_rename2);
 
 int vfs_rename(struct inode *old_dir, struct dentry *old_dentry,
-	       struct inode *new_dir, struct dentry *new_dentry
-	       )
+	       struct inode *new_dir, struct dentry *new_dentry,
+	       struct inode **delegated_inode, unsigned int flags)
 {
-	return vfs_rename2(NULL, old_dir, old_dentry, new_dir, new_dentry);
+	return vfs_rename2(NULL, old_dir, old_dentry, new_dir, new_dentry, delegated_inode, flags);
 }
 EXPORT_SYMBOL(vfs_rename);
 
