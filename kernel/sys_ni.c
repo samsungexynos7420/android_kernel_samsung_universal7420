a7eab83cd9f4ee0343d4fbbd2a850f2ce441b498
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2025-09-10 14:13:51 +0100

    fixup!
    bpf already declared

--- kernel/sys_ni.c
+++ kernel/sys_ni.c
@@ -194,7 +194,6 @@ cond_syscall(compat_sys_timerfd_gettime);
 cond_syscall(sys_eventfd);
 cond_syscall(sys_eventfd2);
 cond_syscall(sys_memfd_create);
-cond_syscall(sys_bpf);
 
 /* performance counters: */
 cond_syscall(sys_perf_event_open);
