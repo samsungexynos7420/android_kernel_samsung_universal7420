0557f4f3e86c14d5ac75a35ec770bf7fd3f91a5e
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2025-11-29 16:45:49 +0000

    Backport  PAGE_BOOST from 7885

--- fs/namespace.c
+++ fs/namespace.c
@@ -1644,6 +1644,10 @@ int path_umount(struct path *path, int flags)
  * unixes. Our API is identical to OSF/1 to avoid making a mess of AMD
  */
 
+#ifdef CONFIG_PAGE_BOOST_RECORDING
+#include <linux/io_record.h>
+#endif
+
 SYSCALL_DEFINE2(umount, char __user *, name, int, flags)
 {
 	struct path path;
@@ -1656,6 +1660,10 @@ SYSCALL_DEFINE2(umount, char __user *, name, int, flags)
 
 	if (!may_mount())
 		return -EPERM;
+
+#ifdef CONFIG_PAGE_BOOST_RECORDING
+		forced_init_record();
+#endif
 
 	if (!(flags & UMOUNT_NOFOLLOW))
 		lookup_flags |= LOOKUP_FOLLOW;
