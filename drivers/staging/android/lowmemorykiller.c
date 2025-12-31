7fcfe0ddb531c48cbe66528dd7ae50df1c89588b
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2025-12-31 16:33:04 +0000

    Fix up

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -237,7 +237,7 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 		rcu_read_unlock();
 	}
 
-	lowmem_print(4, "lowmem_shrink %lu, %x, return %d\n",
+	lowmem_print(4, "lowmem_scan %lu, %x, return %lu\n",
 		     sc->nr_to_scan, sc->gfp_mask, rem);
 	return rem;
 }
