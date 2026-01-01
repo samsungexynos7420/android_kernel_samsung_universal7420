442cd5d6ec77e3de9e389b1ba1dc24c774793357
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2026-01-01 16:01:09 +0000

    fs: remove SCFS
    Old, outdated and unused. get rid of it

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -1422,17 +1422,6 @@ void free_hot_cold_page(struct page *page, bool cold)
 	unsigned long flags;
 	int migratetype;
 
-#ifdef CONFIG_SCFS_LOWER_PAGECACHE_INVALIDATION
-	/*
-	   struct scfs_sb_info *sbi;
-
-	   if (PageScfslower(page) || PageNocache(page)) {
-	   sbi = SCFS_S(page->mapping->host->i_sb);
-	   sbi->scfs_lowerpage_reclaim_count++;
-	   }
-	 */
-#endif
-
 	if (!free_pages_prepare(page, 0))
 		return;
 
@@ -6462,10 +6451,6 @@ static const struct trace_print_flags pageflag_names[] = {
 #endif
 #ifdef CONFIG_TRANSPARENT_HUGEPAGE
 	{1UL << PG_compound_lock,	"compound_lock"	},
-#endif
-#ifdef CONFIG_SCFS_LOWER_PAGECACHE_INVALIDATION
-	{1UL << PG_scfslower, "scfslower"},
-	{1UL << PG_nocache,"nocache"},
 #endif
 };
 
