e88ec78cba2c37b3b6412a410ae45112e5584165
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2026-01-12 17:39:40 +0000

    usb: fix some indentation warnings

--- drivers/usb/gadget/u_ether.c
+++ drivers/usb/gadget/u_ether.c
@@ -777,14 +777,14 @@ static int alloc_tx_buffer(struct eth_dev *dev)
 		if (!req->buf)
 			req->buf = kmalloc(dev->tx_req_bufsize,
 						GFP_ATOMIC);
-			if (!req->buf)
-				goto free_buf;
+		if (!req->buf)
+			goto free_buf;
 #ifdef CONFIG_USB_NCM_ACCUMULATE_MULTPKT
-			if(dev->port_usb->is_fixed) {
-				memcpy(req->buf,dev->port_usb->header,dev->port_usb->header_len);
-				req->length = dev->port_usb->header_len;
-				DEBUG_NCM(KERN_ERR"usb: request(%p) req->len(%d) \n",req,req->length);
-			}
+		if(dev->port_usb->is_fixed) {
+			memcpy(req->buf,dev->port_usb->header,dev->port_usb->header_len);
+			req->length = dev->port_usb->header_len;
+			DEBUG_NCM(KERN_ERR"usb: request(%p) req->len(%d) \n",req,req->length);
+		}
 #endif
 	}
 	return 0;
