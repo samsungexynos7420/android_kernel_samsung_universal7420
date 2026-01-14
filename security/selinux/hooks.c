e60ac8675a45729822f577087def7e307ebf1946
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-01-14 19:13:58 +0800

    KernelSU, selinux: expose selinux_ops for pseudo-LSM-stacking
    since 3.x kernels doesnt have the same capability as > 4.1 to stack LSM's
    we introduce a way to "stack" hooks on top of selinux LSM.
    
    security_operations is just a struct of function pointers.
    
    ```
    struct security_operations {
    	...
    
    	int (*ptrace_access_check) (struct task_struct *child, unsigned int mode);
    	int (*ptrace_traceme) (struct task_struct *parent);
    	...
    ```
    
    and selinux owns one which has
    
    ```
    static struct security_operations selinux_ops = {
    	...
    
    	.ptrace_access_check =		selinux_ptrace_access_check,
    	.ptrace_traceme =		selinux_ptrace_traceme,
    	...
    ```
    
    so as long as we can get the address of selinux_ops we can
    1. redirect actual operation to a fn_pointer we control
    2. create a hook_fn which hooks on entry and returns original operation
    3. redirect the operation to a hook_fn we control
    
    this requires an exposed selinux ops so you need to do:
    
    ```
    --- a/security/selinux/hooks.c
    +++ b/security/selinux/hooks.c
    @@ -5808,7 +5808,7 @@
    
     #endif
    
    -static struct security_operations selinux_ops = {
    +struct security_operations selinux_ops = {
     	.name =				"selinux",
    
     	.binder_set_context_mgr =	selinux_binder_set_context_mgr,
    ```
    https://github.com/backslashxx/KernelSU/commit/212e0bd109ebbfeb8fcd504545c8e54b01036bea
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -6734,7 +6734,7 @@ static int selinux_key_getsecurity(struct key *key, char **_buffer)
 
 #endif
 
-RKP_RO_AREA static struct security_operations selinux_ops = {
+RKP_RO_AREA struct security_operations selinux_ops = {
 	.name =				"selinux",
 
 	.binder_set_context_mgr =	selinux_binder_set_context_mgr,
