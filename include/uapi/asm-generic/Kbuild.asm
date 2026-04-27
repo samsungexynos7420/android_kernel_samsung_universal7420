cdf1290a790d23187026aa6b9c06a08af7ee293a
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2026-04-27 17:42:11 +0100

    Remove hack for qcom modems
    dont see why it needs to be here :/
    
    Revert "Remove hack for qcom modems"
    
    This reverts commit 9f70df1132105f80cff4c8f06091812502b0f36b.
    
    include fcntl.h

--- include/uapi/asm-generic/Kbuild.asm
+++ include/uapi/asm-generic/Kbuild.asm
@@ -13,7 +13,7 @@ header-y += bitsperlong.h
 header-y += byteorder.h
 header-y += errno.h
 # remove fcntl.h for zero_MDM9x35
-#header-y += fcntl.h
+header-y += fcntl.h
 header-y += ioctl.h
 header-y += ioctls.h
 header-y += ipcbuf.h
