009382537509bac1bfda0eefd833f27fbe429c87
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2026-05-20 20:39:44 +0100

    mfc: remove version 5
    this is completely unused in this tree

--- drivers/media/platform/exynos/mfc/s5p_mfc_common.h
+++ drivers/media/platform/exynos/mfc/s5p_mfc_common.h
@@ -1228,11 +1228,7 @@ static inline int s5p_mfc_ctx_ready(struct s5p_mfc_ctx *ctx)
 }
 
 
-#if defined(CONFIG_EXYNOS_MFC_V5)
-#include "regs-mfc-v5.h"
-#include "s5p_mfc_opr_v5.h"
-#include "s5p_mfc_shm.h"
-#elif defined(CONFIG_EXYNOS_MFC_V6)
+#if defined(CONFIG_EXYNOS_MFC_V6)
 #include "regs-mfc-v6.h"
 #include "s5p_mfc_opr_v6.h"
 #elif defined(CONFIG_EXYNOS_MFC_V8)
