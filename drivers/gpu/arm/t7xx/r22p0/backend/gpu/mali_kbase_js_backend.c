cc7550adf996a6dcbc884cc227fa5ec947d6f6c7
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2026-08-26 10:36:37 +0100

    [7420] - Add bounded_to_boot_cluster

--- drivers/gpu/arm/t7xx/r22p0/backend/gpu/mali_kbase_js_backend.c
+++ drivers/gpu/arm/t7xx/r22p0/backend/gpu/mali_kbase_js_backend.c
@@ -313,6 +313,8 @@ int kbase_backend_timer_init(struct kbase_device *kbdev)
 	hrtimer_init(&backend->scheduling_timer, CLOCK_MONOTONIC,
 							HRTIMER_MODE_REL);
 	backend->scheduling_timer.function = timer_callback;
+	/* MALI_SEC_INTEGRATION */
+	backend->scheduling_timer.bounded_to_boot_cluster = true;
 
 	backend->timer_running = false;
 
